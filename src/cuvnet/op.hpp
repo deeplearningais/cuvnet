ce7b415058be67952503dca9d71952fe529423c1
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-01-05 15:32:17 +0100

    implemented and tested Pow functor, topological sort

--- src/cuvnet/op.hpp
+++ src/cuvnet/op.hpp
@@ -22,11 +22,12 @@ namespace cuvnet
                 boost::shared_ptr<op_param<T> >       use(unsigned int i)     { return boost::shared_ptr<op_param<T> >(result_uses[i]); }
                 boost::shared_ptr<const op_param<T> > use(unsigned int i)const{ return boost::shared_ptr<const op_param<T> >(result_uses[i]); }
                 std::vector<boost::weak_ptr<op_param<T>> >   result_uses;
-                boost::weak_ptr<Op> op;
+                boost::shared_ptr<Op> op;
+                std::vector<unsigned int>      shape;
                 //cow_ptr<T>                     value;
                 cow_ptr<T>                     delta;
                 bool                           delta_set;
-                boost::shared_ptr<Op> get_op(){ return boost::shared_ptr<Op>(op); }
+                boost::shared_ptr<Op> get_op(){ return op; }
                 bool want_result()const { return result_uses.size() > 0; }
                 bool can_overwrite_directly()const{
                     if(result_uses.size()!=1)
@@ -46,23 +47,26 @@ namespace cuvnet
                     return use(0)->value;
                 }
                 void push(const cow_ptr<T>& v){
-                    assert(!can_overwrite_directly());
-                    assert(!can_add_directly());
+                    //assert(!can_overwrite_directly());
+                    //assert(!can_add_directly());
                     for (int i = 0; i < result_uses.size(); ++i)
                     {
                         op_param<T>& dst    = *use(i);
                         if(dst.value_set)
                             *dst.value     += v.cdata();
-                        else
+                        else{
                             dst.value       = v;
+                            dst.value_set   = true;
+                        }
                     }
                 }
             };
         template<class T>
             struct op_param{
-                boost::shared_ptr<op_result<T> > use(unsigned int i){ return param_uses[i]; }
-                boost::shared_ptr<const op_result<T> > use(unsigned int i)const{ return param_uses[i]; }
+                boost::shared_ptr<op_result<T> >&       use(unsigned int i){ return param_uses[i]; }
+                boost::shared_ptr<const op_result<T> >  use(unsigned int i)const{ return param_uses[i]; }
                 std::vector<boost::shared_ptr<op_result<T> > >     param_uses;
+                std::vector<unsigned int>      shape;
                 bool                           need_derivative;
                 boost::weak_ptr<Op>            op;
                 cow_ptr<T>                     value;
@@ -88,15 +92,17 @@ namespace cuvnet
                     return use(0)->delta;
                 }
                 void push(const cow_ptr<T>& v){
-                    assert(!can_overwrite_directly());
-                    assert(!can_add_directly());
+                    //assert(!can_overwrite_directly());
+                    //assert(!can_add_directly());
                     for (int i = 0; i < param_uses.size(); ++i)
                     {
-                        op_param<T>& dst    =use(i); 
-                        if(dst.value_set)
-                            dst.value.data() += v.cdata();
-                        else
-                            dst.value       = v;
+                        op_result<T>& dst    = *use(i); 
+                        if(dst.delta_set)
+                            dst.delta.data() += v.cdata();
+                        else{
+                            dst.delta       = v;
+                            dst.delta_set   = true;
+                        }
                     }
                 }
             };
@@ -120,18 +126,45 @@ namespace cuvnet
                 set_n_params(n_params);
                 set_n_results(n_results);
             }
+
+            /** 
+             * returns a reference to the i-th result
+             *
+             * @note the included check is inefficient but avoids making constructor private
+             *       since we cannot determine `shared_from_this' in
+             *       the constructor when we construct objects in m_results.
+             *       I assume that result() will be primarily used to construct
+             *       functions, which is not that often.
+             */
+            result_t&       result(const unsigned int i=0){
+                if(!m_results[i]->op)
+                    m_results[i]->op = shared_from_this();
+                return m_results[i];
+            }
+            /** 
+             * returns a reference to the i-th parameter
+             */
+            param_t&       param(const unsigned int i=0){
+                return m_params[i];
+            }
             void set_n_params(unsigned int n){ 
                 m_params.resize(n); 
-                for(int i=0;i<n;i++)
-                    m_params[i]->op = shared_from_this();
+                for(int i=0;i<n;i++){
+                    m_params[i].reset(new detail::op_param<value_type>());
+                }
             }
             void set_n_results(unsigned int n){ 
                 m_results.resize(n); 
-                for(int i=0;i<n;i++)
-                    m_results[i]->op = shared_from_this();
+                for(int i=0;i<n;i++){
+                    m_results[i].reset(new detail::op_result<value_type>());
+                }
             }
             unsigned int get_n_params(){ return m_params.size(); }
             unsigned int get_n_results(){ return m_results.size(); }
+            void add_param(unsigned int idx, result_t& p){
+                param(idx)->param_uses.push_back(p);
+                p->result_uses.push_back(param(idx));
+            }
 
 			/**
 			 * calculate recursively what needs to be calculated to
@@ -141,19 +174,15 @@ namespace cuvnet
 			 *
 			 * @param l the list of parameters w.r.t. which this op is to be derived
 			 */
-            template<class T>
-			inline bool set_calculate_derivative(const std::list<T>&l){
-                if(l.end() != std::find(l.begin(),l.end(), 
-                            std::bind2nd(
-                                std::equal_to<T>(),
-                                (T)shared_from_this()))){
+			inline bool set_calculate_derivative(const std::list<Op*>&l){
+                if(l.end() != std::find(l.begin(),l.end(), this)){
                     assert(m_params.size()==0); // this should be a "scalar"
                     return true;
                 }
                 bool need_calc_derivative = false;
                 BOOST_FOREACH(param_t& p, m_params){
                     bool derive_wrt_p = false;
-                    BOOST_FOREACH(detail::op_result<value_type>* r, p->param_uses){
+                    BOOST_FOREACH(Op::result_t& r, p->param_uses){
                         derive_wrt_p |= r->get_op()->set_calculate_derivative(l);
                     }
                     p->need_derivative = derive_wrt_p;
@@ -162,36 +191,200 @@ namespace cuvnet
                 return need_calc_derivative;
 			}
 
-            struct param_collector{
+            /**
+             * helper class to create visitors (you can derive from this so
+             * that you e.g. only need to implement one method)
+             */
+            struct op_visitor_adaptor{
+                inline bool discover(Op* o)const{ return true; }
+                inline void preorder(Op* o)const{ ; }
+                inline void postorder(Op* o)const{ ; }
+            };
+            /**
+             * collect all no-input ops in a list
+             */
+            struct param_collector_visitor : public op_visitor_adaptor{
                 std::list<Op*>     plist;
                 std::map<Op*,bool> visited;
-                inline bool preorder(Op* o){
+                inline bool discover(Op* o){
                     if(visited.find(o)!=visited.end())
-                        return true;
+                        return false;
                     visited[o]=true;
+                    return true;
+                }
+                inline void preorder(Op* o){
                     if(o->get_n_params()==0)
                         plist.push_back(o);
-                    return true;
-                }
-                inline bool postorder(Op* o){
-                    return true;
                 }
             };
-            template<class Visitor>
-            bool visit(Visitor& v){
-                if(!v.preorder(this)) return false;
-                BOOST_FOREACH(param_t& p, m_params){
-                    BOOST_FOREACH(boost::shared_ptr<detail::op_result<value_type> > r, p->param_uses){
-                        if(!r->get_op()->visit(v))
-                            return false;
+            /**
+             * collect all ops in a list in topological order
+             */
+            struct toposort_visitor : public op_visitor_adaptor{
+                std::vector<Op*>     plist;
+                std::map<Op*,bool> visited;
+                bool               deriv_only;
+                toposort_visitor(bool deriv):deriv_only(deriv){}
+                inline bool discover(Op* o){
+                    if(visited.find(o)!=visited.end()) return false;
+                    if(deriv_only){
+
+                        if(o->m_params.size()==0) // input
+                            return true;
+                        for (int i = 0; i < o->m_params.size(); ++i)
+                        {
+                            // at least one parameter should have this set
+                            if(o->m_params[i]->need_derivative){
+                                visited[o] = true;
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                inline void postorder(Op* o){
+                    plist.push_back(o);
+                }
+            };
+            /*
+             *[**
+             * * weak pointers to the ops cannot be created in constructors, we
+             * * need to post-hoc wire them.  another alternative is to make
+             * * constructors private as in:
+             * * http://stackoverflow.com/questions/4598986/weak-pointer-to-this-in-constructor
+             * * but this would change the C++-y interface.
+             * *]
+             *struct wire_visitor : public op_visitor_adaptor{
+             *    inline bool preorder(Op* o)const{
+             *        BOOST_FOREACH(Op::result_t& p, o->m_results){
+             *            p->op = o->shared_from_this();
+             *        }
+             *        BOOST_FOREACH(Op::param_t& p, o->m_params){
+             *            p->op = o->shared_from_this();
+             *        }
+             *    }
+             *};
+             */
+
+            /**
+             * determine shapes recursively
+             */
+            struct determine_shapes_visitor :public op_visitor_adaptor{
+                inline void postorder(Op* o)const{
+                    o->_determine_shapes();
+                    // push from result to result-users
+                    BOOST_FOREACH(Op::result_t& r, o->m_results){
+                        for(unsigned int i=0;i<r->result_uses.size();i++){
+                            r->use(i)->shape = r->shape;
+                        }
                     }
                 }
-                return v.postorder(this);
+            };
+
+            /**
+             * show all Ops to a (constant) visitor recursively
+             */
+            template<class Visitor>
+            bool visit(const Visitor& v){
+                if(!v.discover(this)) return true;
+                v.preorder(this);
+                BOOST_FOREACH(Op::param_t& p, m_params){
+                    BOOST_FOREACH(boost::shared_ptr<detail::op_result<value_type> > r, p->param_uses){
+                        r->get_op()->visit(v);
+                    }
+                }
+                v.postorder(this);
+            }
+            /**
+             * show all Ops to a  visitor recursively
+             */
+            template<class Visitor>
+            bool visit(Visitor& v){
+                if(!v.discover(this)) return true;
+                v.preorder(this);
+                BOOST_FOREACH(Op::param_t& p, m_params){
+                    BOOST_FOREACH(boost::shared_ptr<detail::op_result<value_type> > r, p->param_uses){
+                        !r->get_op()->visit(v);
+                    }
+                }
+                v.postorder(this);
             }
 
+            /**
+             * user-supplied function: calculate results of this op
+             */
             virtual void fprop()=0;
+            /**
+             * user-supplied function: backpropagate results of this op
+             */
             virtual void bprop()=0;
+
+            /**
+             * virtual function: determine the shape for each result.
+             *
+             * The default works for ops with only one input:
+             * the shape of the input is simply passed to each result.
+             */
+            virtual void _determine_shapes(){
+                assert(m_params.size()==1);
+                BOOST_FOREACH(result_t& r, m_results){
+                    r->shape = m_params[0]->shape;
+                }
+            }
         };
+
+    class Output
+        : public Op{
+            public:
+                typedef Op::value_type    value_type;
+                typedef Op::op_ptr        op_ptr;
+                typedef Op::value_ptr     value_ptr;
+                typedef Op::param_t       param_t;
+                typedef Op::result_t      result_t;
+
+            private:
+                value_ptr m_data;
+
+            public:
+                Output(result_t& p0):Op(1,0){ 
+                    add_param(0,p0);
+                }
+                void fprop(){
+                    // simply store the inputs
+                    m_data = m_params[0]->value;
+                    m_params[0]->value.reset();
+                }
+                void bprop(){}
+                void _determine_shapes(){ }
+                //value_type&       data()      { return m_data; }
+                const value_type& data() const{ return m_data; }
+        };
+    class Input
+        : public Op{
+            public:
+                typedef Op::value_type    value_type;
+                typedef Op::op_ptr        op_ptr;
+                typedef Op::value_ptr     value_ptr;
+                typedef Op::param_t       param_t;
+                typedef Op::result_t      result_t;
+
+            private:
+                value_ptr m_data;
+
+            public:
+                template<class T>
+                Input(const T& init):Op(0,1), m_data(new value_type(init)){  }
+                void fprop(){
+                    m_results[0]->push(m_data);
+                }
+                void bprop(){}
+                void _determine_shapes(){
+                    m_results[0]->shape = m_data->shape();
+                }
+                value_type&       data()      { return m_data; }
+                const value_type& data() const{ return m_data; }
+        };
+
 
     class Identity 
         : public Op{
@@ -203,7 +396,9 @@ namespace cuvnet
             typedef Op::result_t      result_t;
 
             public:
-            Identity():Op(1,1){}
+            Identity(result_t& p0):Op(1,1){
+                add_param(0,p0);
+            }
 
             void fprop(){
                 // identity
@@ -220,7 +415,7 @@ namespace cuvnet
                 }else{
                     r0->push(p0->value); // 'copy' a newly created matrix
                 }
-                p0->value.reset(); // don't need that for backprop etc.
+                p0->value.reset();       // don't need that for backprop etc.
             }
             void bprop(){
                 // identity
@@ -240,18 +435,6 @@ namespace cuvnet
                 r0->delta.reset();
             }
         };
-
-    class Input
-        : public Op{
-            typedef Op::value_type    value_type;
-            typedef Op::op_ptr        op_ptr;
-            typedef Op::value_ptr     value_ptr;
-            typedef Op::param_t       param_t;
-            typedef Op::result_t      result_t;
-            public:
-            Input():Op(0,1){ }
-        };
-
     class Pow
         : public Op{
             typedef Op::value_type    value_type;
@@ -260,18 +443,77 @@ namespace cuvnet
             typedef Op::param_t       param_t;
             typedef Op::result_t      result_t;
 
+            float m_exponent;
             public:
-            void add_param(unsigned int idx, result_t& p){
-                m_params[idx]->param_uses.push_back(p);
-                p->result_uses.push_back(m_params[idx]);
-            }
-            Pow(float exponent, result_t& p0):Op(1,1){
+            Pow(float exponent, result_t& p0):Op(1,1), m_exponent(exponent){
                 add_param(0,p0);
             }
 
             void fprop(){
+                using namespace cuv;
+                param_t&  p0 = m_params[0];
+                result_t& r0 = m_results[0];
+
+                const value_type& inp = p0->value.cdata();
+                value_ptr res(new value_type(inp.shape()));
+
+                apply_scalar_functor( *res,
+                        inp, SF_POW, m_exponent);
+
+                r0->push(res); // 'copy' a newly created matrix
+
+                if(!p0->need_derivative)
+                    p0->value.reset();       // forget it
             }
             void bprop(){
+                using namespace cuv;
+                param_t&  p0 = m_params[0];
+                result_t& r0 = m_results[0];
+                assert(p0->need_derivative);
+
+                const value_type& inp = p0->value.cdata();
+                value_ptr res(new value_type(inp.shape()));
+                apply_scalar_functor(*res,inp,SF_DPOW, m_exponent);
+                p0->push(res);
+            }
+        };
+    class Tanh
+        : public Op{
+            typedef Op::value_type    value_type;
+            typedef Op::op_ptr        op_ptr;
+            typedef Op::value_ptr     value_ptr;
+            typedef Op::param_t       param_t;
+            typedef Op::result_t      result_t;
+
+            public:
+            Tanh(result_t& p0):Op(1,1){
+                add_param(0,p0);
+            }
+
+            void fprop(){
+                using namespace cuv;
+                param_t&  p0 = m_params[0];
+                result_t& r0 = m_results[0];
+
+                const value_type& inp = p0->value.cdata();           // original
+                value_type&      outp = p0->value.data_onlyshape();  // if detached, only allocate same size storage
+
+                apply_scalar_functor( outp, inp, SF_TANH);
+
+                r0->push(p0->value);      // 'copy' a newly created matrix
+                p0->value.reset(); // forget it
+            }
+            void bprop(){
+                using namespace cuv;
+                param_t&  p0 = m_params[0];
+                result_t& r0 = m_results[0];
+                assert(p0->need_derivative);
+
+                value_type& delta = r0->delta.data();
+
+                apply_scalar_functor(delta,delta,SF_DTANH);
+                r0->push(r0->delta);
+                r0->delta.reset();
             }
         };
 }
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -73,10 +73,70 @@ TEST(ce_ptr_test, init){
 
 }
 
-TEST(op_test,misc){
+TEST(op_test,wiring){
 	typedef boost::shared_ptr<Op> ptr_t;
-    ptr_t id = boost::make_shared<Identity>();
-    Op::param_collector pc;
-    id->visit(pc);
-    EXPECT_EQ(pc.plist.size(),0);
+    ptr_t inp = boost::make_shared<Input>(cuv::extents[10][20]);
+    ptr_t id  = boost::make_shared<Identity>(inp->result());
+
+    // find input object
+    Op::param_collector_visitor pcv;
+    id->visit(pcv);
+    EXPECT_EQ(pcv.plist.size(),1);
+
+    // determine which derivatives to calculate
+    EXPECT_EQ(id->param(0)->need_derivative, false);
+    id->set_calculate_derivative(pcv.plist);
+    EXPECT_EQ(id->param(0)->need_derivative, true);
+
+    // shape propagation
+    id->visit(Op::determine_shapes_visitor());
+    EXPECT_EQ(*pcv.plist.begin(),inp.get());
+    EXPECT_EQ(id->result(0)->shape.at(0), 10);
+    EXPECT_EQ(id->result(0)->shape.at(1), 20);
+}
+
+TEST(op_test,fprop_and_bprop){
+	typedef boost::shared_ptr<Op> ptr_t;
+    boost::shared_ptr<Input>  inp = boost::make_shared<Input>(cuv::extents[10][20]);
+    ptr_t pow                     = boost::make_shared<Pow>(2,inp->result());
+    boost::shared_ptr<Output> out = boost::make_shared<Output>(pow->result());
+
+    boost::dynamic_pointer_cast<Input>(inp)->data() = 2.f;
+
+    // tell that we want derivative w.r.t. all params
+    Op::param_collector_visitor pcv;
+    pow->visit(pcv);
+    EXPECT_EQ(pcv.plist.size(),1);
+    pow->set_calculate_derivative(pcv.plist);
+    EXPECT_EQ(pow->param(0)->need_derivative, true);
+
+    inp->fprop();
+    pow->fprop();
+    out->fprop();
+    EXPECT_EQ(out->data()[0],4);
+
+    pow->bprop();
+    inp->bprop();
+    EXPECT_EQ(inp->result()->delta.cdata()[0],1);
+}
+
+TEST(op_test,toposort){
+	typedef boost::shared_ptr<Op> ptr_t;
+    boost::shared_ptr<Input>  inp = boost::make_shared<Input>(cuv::extents[10][20]);
+    ptr_t pow                     = boost::make_shared<Pow>(2,inp->result());
+    boost::shared_ptr<Output> out = boost::make_shared<Output>(pow->result());
+
+    // tell that we want derivative w.r.t. all params
+    Op::param_collector_visitor pcv;
+    pow->visit(pcv);
+    EXPECT_EQ(pcv.plist.size(),1);
+    pow->set_calculate_derivative(pcv.plist);
+
+    // determine sequence in which to call fwd, bwd pass
+    Op::toposort_visitor tv(true);
+    pow->visit(tv);
+
+    EXPECT_EQ(tv.plist.size(), 2);
+    EXPECT_EQ(tv.plist.at(0), inp.get());
+    EXPECT_EQ(tv.plist.at(1), pow.get());
 }
