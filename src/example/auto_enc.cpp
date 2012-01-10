a02b394fc7098a76c1c78ba62326a4573c64fdf1
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-01-10 18:13:47 +0100

    autoencoder: get rid of macros to define autoencoder loss function

--- src/example/auto_enc.cpp
+++ src/example/auto_enc.cpp
@@ -1,23 +1,15 @@
 // vim:ts=4:sw=4:et
 #include <fstream>
 #include <cmath>
-#include <cuvnet/op.hpp>
 #include <cuvnet/op_utils.hpp>
-#include <cuvnet/ops/output.hpp>
-#include <cuvnet/ops/input.hpp>
-#include <cuvnet/ops/pow.hpp>
-#include <cuvnet/ops/axpby.hpp>
-#include <cuvnet/ops/tanh.hpp>
-#include <cuvnet/ops/prod.hpp>
-#include <cuvnet/ops/noiser.hpp>
-#include <cuvnet/ops/sum.hpp>
+#include <cuvnet/ops.hpp>
+#include <cuv/libs/cimg/cuv_cimg.hpp>
+#include <cuv/tensor_ops/rprop.hpp>
+#include <tools/visualization.hpp>
+#include <datasets/cifar.hpp>
+#include <datasets/mnist.hpp>
 
 using namespace cuvnet;
-typedef boost::shared_ptr<Op> optr_t;
-#define C1(Class,Arg) (boost::make_shared<Class>(Arg)->result())
-#define C2(Class,A1,A2) (boost::make_shared<Class>(A1,A2))->result()
-#define C3(Class,A1,A2,A3) (boost::make_shared<Class>(A1,A2,A3))->result()
-#define C4(Class,A1,A2,A3,A4) (boost::make_shared<Class>(A1,A2,A3,A4))->result()
 
 struct auto_encoder{
     boost::shared_ptr<Input>  m_input;
@@ -31,49 +23,65 @@ struct auto_encoder{
     :m_input(new Input(cuv::extents[inp0][inp1]))
     ,m_weights(new Input(cuv::extents[inp1][hl]))
     {
-        // Sum ( input - tanh( x W ) W' )^2
-        m_func = 
-            boost::make_shared<Sum>(
-                    C2(Pow,2.f,
-                        C4(Axpby,
-                            m_input->result(),
-                            C4(Prod,
-                                C1(Tanh,
-                                    C2(Prod,
-                                        C2(Noiser, m_input->result(), std),
-                                        m_weights->result())),
-                                m_weights->result(),
-                                'n','t'),
-                            1.f,-1.f)));
+        // Sum ( input - tanh( X W ) W' )^2
+        m_func = sum( pow( axpby(1.f,m_input, -1.f, prod(
+                            tanh(prod(
+                                    add_rnd_normal(m_input,std),
+                                    m_weights)),
+                            m_weights, 'n','t')),
+                    2.f));
         m_out = boost::make_shared<Output>(m_func->result());
 
         float diff = 4.f*std::sqrt(6.f/(inp1+hl));
         float mult = 2.f*diff;
-        for(unsigned int i=0;i<m_weights->data().size();i++){
-            m_weights->data()[i] = (float)(drand48()*mult-diff);
-        }
+        cuv::tensor<float,cuv::host_memory_space> x = m_weights->data();
+        for(unsigned int i=0;i<x.size();i++)
+            x[i] = (float)(drand48()*mult-diff);
+        m_weights->data() = x;
     }
 };
 
 int main(int argc, char **argv)
 {
+    // TODO: TODO TODO: Saxpy functor for cuv!
     cuv::initialize_mersenne_twister_seeds();
-    auto_encoder ae(100,8,8,0.0f);
+    mnist_dataset ds("/home/local/datasets/MNIST");
+    unsigned int fa=15,fb=15;
+    auto_encoder ae(ds.val_data.shape(0),ds.val_data.shape(1),fa*fb,0.05f);
+    {
+        cuv::tensor<float,cuv::host_memory_space> w = ae.m_weights->data();
+        cuv::tensor<float,cuv::host_memory_space> wt(cuv::extents[w.shape(1)][w.shape(0)]);
+        wt = 0.f;
+        cuv::transpose(wt,w);
+        wt -= cuv::minimum(wt);
+        wt /= cuv::maximum(wt);
+        cuv::libs::cimg::show(arrange_filters(wt, fa, fb, ds.image_size,ds.channels), "weights");
+    }
 
     // generate data
-    for(unsigned int i=0;i<ae.input().size();i++)
-        ae.input()[i] = 2.f * (float)((int)(drand48()*10)%2)-1.0f;
-
+    ae.input()  = ds.val_data;
+    ae.input() -= cuv::mean(ae.input());
+    ae.input() /= std::sqrt(cuv::var(ae.input()));
+    
     std::vector<Op*> params(1,ae.m_weights.get());
     swiper swipe(*ae.m_func,true,params);
 
-    for(unsigned int epoch=0;epoch<1000;epoch++){
+    for(unsigned int epoch=0;epoch<2000;epoch++){
         swipe.fprop();
-        std::cout << std::sqrt(ae.output()[0]/ae.input().shape(1))<<std::endl;
+        std::cout << epoch << " "<<std::sqrt(ae.output()[0]/ae.input().shape(1)/ae.input().shape(0))<<" "<<cuv::norm2(ae.m_weights->data())<<std::endl;
         swipe.bprop();
 
-        ae.m_weights->data() -= 0.001f*ae.m_weights->result()->delta.cdata();
+        cuv::learn_step_weight_decay(
+                ae.m_weights->data(),
+                const_cast<matrix&>(ae.m_weights->result()->delta.cdata()),
+                -0.001f/ds.val_data.shape(0),.001f);
     }
 
+    cuv::tensor<float,cuv::host_memory_space> w = ae.m_weights->data();
+    cuv::tensor<float,cuv::host_memory_space> wt(cuv::extents[w.shape(1)][w.shape(0)]);
+    cuv::transpose(wt,w);
+    wt -= cuv::minimum(wt);
+    wt /= cuv::maximum(wt);
+    cuv::libs::cimg::show(arrange_filters(wt, fa,fb, ds.image_size,ds.channels), "weights");
     return 0;
 }
