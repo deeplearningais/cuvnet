8c7a89c765b85c4cc75607129670a33127a551a2
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-01-14 19:26:42 +0100

    clean up AE

--- src/example/auto_enc.cpp
+++ src/example/auto_enc.cpp
@@ -29,7 +29,7 @@ struct auto_encoder{
 
     void print_loss(unsigned int epoch){ std::cout << epoch<<" " << output()[0]<<std::endl;}
 
-    auto_encoder(unsigned int inp0, unsigned int inp1, unsigned int hl, float noise=0.0f, float lambda=0.0f)
+    auto_encoder(unsigned int inp0, unsigned int inp1, unsigned int hl, bool binary, float noise=0.0f, float lambda=0.0f)
     :m_input(new Input(cuv::extents[inp0][inp1],"input"))
     ,m_weights(new Input(cuv::extents[inp1][hl],"weights"))
     ,m_bias_h(new Input(cuv::extents[hl],       "bias_h"))
@@ -43,22 +43,21 @@ struct auto_encoder{
                 prod( m_enc, m_weights, 'n','t')
                 ,m_bias_y,1);
 
-        // squared reconstruction loss
-        m_rec_loss    = mean( pow( axpby(m_input, -1.f, m_decode), 2.f)); // reconstruction loss (squared diff)
-        // negative cross-entropy loss of logistic (stable/fast version)
-        //m_rec_loss    = mean( sum(neg_log_cross_entropy_of_logistic(m_input,m_decode),1)); // reconstruction loss (cross-entropy)
+        if(!binary)  // squared loss
+            m_rec_loss = mean( pow( axpby(m_input, -1.f, m_decode), 2.f)); 
+        else         // cross-entropy
+            m_rec_loss = mean( sum(neg_log_cross_entropy_of_logistic(m_input,m_decode),1));
         m_out         = make_shared<Output>(m_rec_loss->result()); // reconstruction error
         m_reconstruct = make_shared<Output>(m_decode->result());   // for visualization of reconstructed images
-        //m_corrupt = make_shared<Output>(corrupt->result());
+        //m_corrupt = make_shared<Output>(corrupt->result());      // for visualization of corrupted     images
 
-        if(lambda>0.f){
+        if(lambda>0.f){ // contractive AE
             m_contractive_loss = 
                 sum(sum(pow(m_enc*(1.f-m_enc),2.f),0) 
                         * sum(pow(m_weights,2.f),0));
             m_loss        = axpby(m_rec_loss, lambda/(float)inp0, m_contractive_loss);
-        }else{
-            m_loss        = m_rec_loss;
-        }
+        }else
+            m_loss        = m_rec_loss; // no change
 
         // initialize weights and biases
         float diff = 4.f*std::sqrt(6.f/(inp1+hl));
@@ -71,28 +70,27 @@ struct auto_encoder{
 };
 
 void load_batch(
-        auto_encoder& ae,
-        cuv::tensor<float,cuv::dev_memory_space>& data,
+        auto_encoder* ae,
+        cuv::tensor<float,cuv::dev_memory_space>* data,
         unsigned int bs, unsigned int batch){
-    ae.input().set_view(cuv::indices[cuv::index_range(batch*bs,(batch+1)*bs)][cuv::index_range()], data);
+    ae->input().set_view(cuv::indices[cuv::index_range(batch*bs,(batch+1)*bs)][cuv::index_range()], *data);
 }
 
 int main(int argc, char **argv)
 {
     cuv::initialize_mersenne_twister_seeds();
-#if 1
     {   // check auto-encoder derivatives
-        auto_encoder ae(2,4,2,0.0f);
+        auto_encoder ae(2,4,2,false,0.0f,0.2f);
         derivative_tester_verbose(*ae.m_enc);
         derivative_tester_verbose(*ae.m_decode);
         derivative_tester_verbose(*ae.m_rec_loss);
-        if(ae.m_contractive_loss.get())
+        if(ae.m_contractive_loss.get()){
             derivative_tester_verbose(*ae.m_contractive_loss);
+        }
     }
-#endif
 
     //mnist_dataset ds("/home/local/datasets/MNIST");
-    //global_min_max_normalize<> normalizer; // 0,1
+    //global_min_max_normalize<> normalizer(0,1); // 0,1
     cifar_dataset ds;
     zero_mean_unit_variance<> normalizer;
     
@@ -100,23 +98,29 @@ int main(int argc, char **argv)
     normalizer.transform(ds.val_data);
 
     unsigned int fa=16,fb=16,bs=20;
-
-    auto_encoder ae(bs==0?ds.val_data.shape(0):bs,
-            ds.train_data.shape(1), fa*fb, 0.00f, 0.00f);
+    auto_encoder ae(bs==0?ds.train_data.shape(0):bs,
+            ds.train_data.shape(1), fa*fb, 
+            ds.channels==1, 0.00f, 0.01f);
 
     std::vector<Op*> params;
     params += ae.m_weights.get(), ae.m_bias_y.get(), ae.m_bias_h.get();
 
-    Op::value_type alldata = bs==0 ? ds.val_data : ds.train_data;
-    gradient_descent gd(ae.m_loss,params);
+    Op::value_type alldata = bs==0 ? ds.train_data : ds.train_data;
+    rprop_gradient_descent gd(ae.m_loss,params);
     gd.after_epoch.connect(boost::bind(&auto_encoder::print_loss, &ae, _1));
-    gd.before_batch.connect(boost::bind(load_batch,ae,alldata,bs,_2));
-    if(bs==0) gd.batch_learning(200);
+    gd.before_batch.connect(boost::bind(load_batch,&ae,&alldata,bs,_2));
+    if(bs==0){
+        ae.input()= alldata;
+        gd.batch_learning(400);
+    }
     else      gd.minibatch_learning(200, ds.train_data.shape(0)/bs);
     
     // show the resulting filters
-    cuv::libs::cimg::show(arrange_filters(ae.m_reconstruct->cdata(),'n', (int)sqrt(bs),(int)sqrt(bs), ds.image_size,ds.channels), "input");
+    unsigned int n_rec = (bs>0) ? sqrt(bs) : 6;
+    cuv::libs::cimg::show(arrange_filters(ae.m_reconstruct->cdata(),'n', n_rec,n_rec, ds.image_size,ds.channels), "input");
     auto wvis = arrange_filters(ae.m_weights->data(), 't', fa, fb, ds.image_size,ds.channels,false);
     cuv::libs::cimg::save(wvis, "contractive-weights.png");
+    wvis      = arrange_filters(ae.m_weights->data(), 't', fa, fb, ds.image_size,ds.channels,true);
+    cuv::libs::cimg::save(wvis, "contractive-weights-sepn.png");
     return 0;
 }
