86201c3a6c6b44b9c7818a711f851b7993598e99
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-03-06 15:44:23 +0100

    add whitening/pca/zca preprocessing

--- src/tools/preprocess.hpp
+++ src/tools/preprocess.hpp
@@ -5,6 +5,19 @@
 #include<cuv/matrix_ops/matrix_ops.hpp>
 #include<cuv/tensor_ops/tensor_ops.hpp>
 
+/// for PCA whitening
+#define BOOST_UBLAS_SHALLOW_ARRAY_ADAPTOR  1
+#include <boost/numeric/ublas/storage.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/banded.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/bindings/lapack/syev.hpp>
+#include <boost/numeric/bindings/traits/ublas_matrix.hpp>
+#include <boost/numeric/bindings/traits/ublas_vector.hpp> 
+
+#include "argsort.hpp"
 namespace cuvnet
 {
     namespace detail
@@ -82,6 +95,108 @@ namespace cuvnet
                 fit(data); transform(data);
             }
     };
+
+    class pca_whitening{
+        private:
+            int m_n_components;
+            zero_mean_unit_variance<cuv::host_memory_space> m_zm;
+            cuv::tensor<float,cuv::host_memory_space> m_rot;
+
+            // shorthands
+            typedef float real;
+            typedef boost::numeric::ublas::shallow_array_adaptor<real>  ArrayAdaptor; 
+            typedef boost::numeric::ublas::matrix<real,boost::numeric::ublas::row_major,ArrayAdaptor>    adaptor_matrix; 
+            typedef boost::numeric::ublas::matrix_column<adaptor_matrix>    acolumn; 
+            typedef boost::numeric::ublas::matrix<real,boost::numeric::ublas::column_major> ubmatrix;
+            typedef boost::numeric::ublas::matrix_column<ubmatrix>            column; 
+            typedef boost::numeric::ublas::matrix_column<ubmatrix>            row; 
+            typedef boost::numeric::ublas::vector<real>                     vector;
+            typedef boost::numeric::ublas::scalar_vector<real>              scalar_vector;
+            typedef boost::numeric::ublas::range                            range;
+
+            bool m_whiten, m_zca;
+
+        public:
+            
+
+            pca_whitening(int n_components=-1, bool whiten=true, bool zca=false):m_n_components(n_components),m_zm(false),m_whiten(whiten),m_zca(zca){}
+            void fit(const cuv::tensor<float,cuv::host_memory_space>& train_){
+                namespace lapack = boost::numeric::bindings::lapack;
+                namespace ublas  = boost::numeric::ublas;
+                if(m_n_components<=0)
+                    m_n_components = train_.shape(1);
+
+                // ------- determine covariance matrix ----------
+                //  subtract mean
+                cuv::tensor<float,cuv::host_memory_space> train = train_.copy();
+                m_zm.fit_transform(train);
+
+                // calculate covariance matrix
+                cuv::tensor<float,cuv::host_memory_space> cov(cuv::extents[train_.shape(1)][train_.shape(1)]);
+                cuv::prod(cov,train,train,'t','n');
+                cov /= (float)train.shape(0);
+
+                // create UBLAS adaptors 
+                m_rot.resize(cuv::extents[train.shape(1)][m_n_components]);
+                adaptor_matrix base_data(train.shape(0),train.shape(1),ArrayAdaptor(train.size(),const_cast<real*>(train.ptr())));
+                adaptor_matrix ucov      (train.shape(1),train.shape(1),ArrayAdaptor(cov.size(),const_cast<real*>(cov.ptr())));
+                adaptor_matrix rot      (train.shape(1),m_n_components,ArrayAdaptor(m_rot.size(),const_cast<real*>(m_rot.ptr())));
+
+                vector S(train.shape(1));
+                ubmatrix Eigv(ucov);
+                vector lambda(train.shape(1));
+                lapack::syev( 'V', 'L', Eigv, lambda, lapack::optimal_workspace() );
+                std::vector<unsigned int> idx = argsort(lambda.begin(), lambda.end(), std::greater<float>());
+                sort(lambda.begin(), lambda.end(), std::greater<float>());
+
+                // calculate the cut-off Eigv and diagMatrix matrix 
+                ublas::diagonal_matrix<real> diagMatrix(m_n_components);
+                static const float epsilon = 0.00001f;
+                if(m_whiten)
+                    for(int i=0;i<m_n_components; i++){
+                        real r = lambda(i) + epsilon;
+                        diagMatrix(i,i) = 1.0/sqrt(r);
+                    }
+                else
+                    for(int i=0;i<m_n_components; i++){
+                        diagMatrix(i,i) = 1.f;
+                    }
+                for(int i=0;i<m_n_components;i++) {
+                    boost::numeric::ublas::matrix_column<adaptor_matrix>(rot,i) = column(Eigv,idx[i]);
+                }
+
+                if(m_zca){
+                    // ZCA whitening:
+                    // Rot = rot * diagMatrix * rot'
+                    ubmatrix tmp = ublas::prod(rot, diagMatrix);
+                    rot = ublas::prod(tmp, ublas::trans(rot) );
+                }
+                else{
+                    // PCA whitening:
+                    // Rot = rot * diagMatrix
+                    rot = ublas::prod(rot, diagMatrix);
+                }
+            }
+            void transform(cuv::tensor<float,cuv::host_memory_space>& data){
+                m_zm.transform(data);
+                cuv::tensor<float,cuv::host_memory_space> res(cuv::extents[data.shape(0)][m_n_components]);
+                cuv::prod( res, data, m_rot );
+                data = res;
+            }
+            void fit_transform(cuv::tensor<float,cuv::host_memory_space>& data){
+                fit(data); transform(data);
+            }
+            void reverse_transform(cuv::tensor<float,cuv::host_memory_space>& res){
+                cuv::tensor<float,cuv::host_memory_space> data(cuv::extents[res.shape(0)][m_rot.shape(0)]);
+                cuv::prod( data, res, m_rot, 'n','t' );
+                m_zm.reverse_transform(data);
+                res = data;
+            }
+
+            inline
+                const cuv::tensor<float,cuv::host_memory_space>& rot(){return m_rot;};
+    };
+
     class preprocessor{
         public:
             virtual void process_filestring(cuv::tensor<float,cuv::host_memory_space>& dst, const char* buf, size_t n)=0;
