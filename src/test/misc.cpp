84e4e071b6ce24fce66a810b3ef05b54e14710b6
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-03-09 11:17:33 +0100

    unit tests for pca,whitening reworked

--- src/test/misc.cpp
+++ src/test/misc.cpp
@@ -5,6 +5,8 @@
 #include <tools/argsort.hpp>
 #include <tools/preprocess.hpp>
 
+#define V(X) #X<<":"<<(X)<<", "
+
 double nrand(){
     double  sum, f;
     int k;
@@ -56,15 +58,15 @@ TEST(pca,normal){
     EXPECT_EQ(data.shape(0),data2.shape(0));
     EXPECT_EQ(data.shape(1),data2.shape(1));
 
-    const tens_t& rot = pp.rot();
-    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
-    cuv::prod(res,rot,rot,'n','t');
-
     // check for identity matrix
+    const tens_t&  rot = pp.rot();
+    const tens_t& rrot = pp.rrot();
+    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
+    cuv::prod(res,rot,rrot);
     for(unsigned int i=0;i<rot.shape(0);i++){
         res(i,i)-=1.f;
     }
-    EXPECT_NEAR(0.f, cuv::sum(res), 0.01f);
+    EXPECT_NEAR(0.f, cuv::norm2(res), 0.01f);
 
     // check for reverse_transform
     pp.reverse_transform(data);
@@ -100,12 +102,7 @@ TEST(pca,reduced_pca){
     EXPECT_EQ(data.shape(0),data2.shape(0));
     EXPECT_EQ(data.shape(1),2);
 
-    const tens_t& rot = pp.rot();
-    EXPECT_EQ(rot.shape(0),data2.shape(1)); // shape of original data
-    EXPECT_EQ(rot.shape(1),            2 );
-
-    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
-    cuv::prod(res,rot,rot,'n','t');
+    // check for identity matrix does not make sense (reduced!)
 
     // check for reverse_transform
     pp.reverse_transform(data);
@@ -142,12 +139,7 @@ TEST(pca,reduced_whiten){
     EXPECT_EQ(data.shape(0),data2.shape(0));
     EXPECT_EQ(data.shape(1),2);
 
-    const tens_t& rot = pp.rot();
-    EXPECT_EQ(rot.shape(0),data2.shape(1)); // shape of original data
-    EXPECT_EQ(rot.shape(1),            2 );
-
-    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
-    cuv::prod(res,rot,rot,'n','t');
+    // check for identity matrix  not possible (not lossless)
 
     // check for reverse_transform
     pp.reverse_transform(data);
@@ -159,6 +151,50 @@ TEST(pca,reduced_whiten){
         }
 }
 
+TEST(pca,normal_zca){
+    typedef cuv::tensor<float,cuv::host_memory_space> tens_t;
+
+    cuvnet::pca_whitening pp(-1,true,true); // 1 less than data dim
+
+    tens_t data (1000, 3);
+
+    // two basis vectors for dataset
+    float p[] = { 2,-1, 1};
+    float q[] = { 1, 3, 5};
+
+    for (unsigned int i = 0; i < data.shape(0); ++i)
+    {
+        float fp = nrand();
+        float fq = nrand(); // less variance in Q
+        data(i,0) = fp*p[0] + fq*q[0] + 4;
+        data(i,1) = fp*p[1] + fq*q[1] + 5;
+        data(i,2) = fp*p[2] + fq*q[2] + 6;
+    }
+    tens_t data2 = data.copy();
+
+    pp.fit_transform(data);
+    EXPECT_EQ(data.shape(0),data2.shape(0));
+    EXPECT_EQ(data.shape(1),data2.shape(1));
+
+    // check for identity matrix
+    const tens_t&  rot = pp.rot();
+    const tens_t& rrot = pp.rrot();
+    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
+    cuv::prod(res,rot,rrot);
+    for(unsigned int i=0;i<rot.shape(0);i++){
+        res(i,i)-=1.f;
+    }
+    EXPECT_NEAR(0.f, cuv::norm2(res), 0.01f);
+
+    // check for reverse_transform
+    pp.reverse_transform(data);
+    EXPECT_TRUE(cuv::equal_shape(data,data2));
+    for(unsigned int i=0;i<data.shape(0);i++)
+        for(unsigned int j=0;j<data.shape(1);j++){
+            //std::cout << "i, j = "<<i<<", "<<j<<std::endl;
+            EXPECT_NEAR(data(i,j), data2(i,j),0.01f);
+        }
+}
 TEST(pca,reduced_zca){
     typedef cuv::tensor<float,cuv::host_memory_space> tens_t;
 
@@ -182,13 +218,9 @@ TEST(pca,reduced_zca){
 
     pp.fit_transform(data);
     EXPECT_EQ(data.shape(0),data2.shape(0));
-    EXPECT_EQ(data.shape(1),2);
+    EXPECT_EQ(data.shape(1),data2.shape(1));
 
-    const tens_t& rot = pp.rot();
-    EXPECT_EQ(rot.shape(0),data2.shape(1)); // shape of original data
-    EXPECT_EQ(rot.shape(1),            2 );
-    tens_t res(cuv::extents[rot.shape(0)][rot.shape(0)]);
-    cuv::prod(res,rot,rot,'n','t');
+    // check for identity matrix not possible: not lossless!
 
     // check for reverse_transform
     pp.reverse_transform(data);
