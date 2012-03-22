6da4e44338ed6536cb6df0d5a316f82d62c911ea
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-03-22 19:17:27 +0100

    preprocessing for natural ds

--- src/tools/learner.cpp
+++ src/tools/learner.cpp
@@ -85,23 +85,23 @@ namespace cuvnet
             // TODO: this one has complicated pre-processing, needs normalizer
             // to be accessible for filter visualization
             dsall         = randomizer().transform(dsall);
-            {
-                global_min_max_normalize<> normalizer(0,1);
-                normalizer.fit_transform(dsall.train_data);
-                normalizer.transform(dsall.test_data);
-            }
+            /*
+             *{
+             *    global_min_max_normalize<> normalizer(0,1);
+             *    normalizer.fit_transform(dsall.train_data);
+             *    normalizer.transform(dsall.test_data);
+             *}
+             */
             {
                 zero_sample_mean<> zsm;
                 zsm.fit_transform(dsall.train_data);
                 zsm.transform(dsall.test_data);
             }
-            /*
-             *{
-             *    zero_mean_unit_variance<> zmuv;
-             *    zmuv.fit_transform(dsall.train_data);
-             *    zmuv.transform(dsall.test_data);
-             *}
-             */
+            {
+                zero_mean_unit_variance<> zmuv;
+                zmuv.fit_transform(dsall.train_data);
+                zmuv.transform(dsall.test_data);
+            }
             m_splits.init(dsall, nsplits);
         }else if(ds == "cifar"){
             dataset dsall = cifar_dataset();
