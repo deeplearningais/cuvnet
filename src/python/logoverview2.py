8e93584361bd298346d84e0a5578334b2c3d954d
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-05-25 15:32:49 +0200

    cleaned up plotting a bit

--- src/python/logoverview2.py
+++ src/python/logoverview2.py
@@ -1,11 +1,15 @@
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import rgb2hex
+import pymongo
 from pymongo import Connection
 import gridfs
 from mnist import MNIST_data
 from jacobs import jacobian_2l
 from progressbar import ProgressBar
+from matplotlib import cm
+import os
 
 from matplotlib.font_manager import FontProperties
 smallFont = FontProperties()
@@ -140,9 +144,23 @@ class experiment:
         self.payload = params["payload"]
         self.conf = params["payload"]["conf"]
         self.result = params["result"]
-        self.log = params["log"]
         self.want_weights = False
-        self.parse_log()
+        self.log = None
+        self.opt = "opt" in params and params["opt"]
+        self.taskid = params["_id"]
+        if "dont-rewrite-l" not in self.conf:
+            self.rewrite_lambda()
+
+    def rewrite_lambda(self):
+        bs = self.conf["bs"]
+        for layer in self.conf["stack"]:
+            if layer["twolayer"]:
+                l = layer["lambda"]
+                layer["lambda"] = l / bs
+
+    def load_log(self, logs):
+        self.log = logs.find(
+                {"taskid": self.taskid}).sort("nr", pymongo.ASCENDING)
 
     def calculate_jacobian(self):
         if not self.want_weights:
@@ -203,45 +221,52 @@ class experiment:
         AE = Lines("AE")
         MLP = Lines("MLP")
         for line in log:
-            if "param" in line:
+            msg = line["msg"]
+            if "param" in msg:
                 if self.want_weights or show_weights:
                     # self.weights will contain the last
                     # occurrence of the name, hopefully from trainall
-                    x = np.fromstring(
-                            gfs.get_last_version(line["filename"]).read(),
-                            dtype="float32").reshape(line["shape"])
-                    self.weights[line["param"] + "-" + line["desc"]] = x
+                    try:
+                        x = np.fromstring(
+                                gfs.get_last_version(line["filename"]).read(),
+                                dtype="float32").reshape(msg["shape"])
+                        self.weights[msg["param"] + "-" + msg["desc"]] = x
+                    except:
+                        print "Failed to load params `%s' (%s)" \
+                                % (msg["param"], msg["desc"])
+                        continue
                 if show_weights:
                     #if "pretrain" not in line["desc"]:
                         #continue
-                    show_filters(x.T, line["param"], line["desc"])
+                    show_filters(x.T, msg["param"], msg["desc"])
+                    plt.show()
 
-            if "validation_mode" in line:
-                valmode = ["training", "validation"][line["validation_mode"]]
-            if "topic" in line:
-                if line["topic"] == "switch_dataset":
-                    if line["mode"] == 1:  # CM_TRAINALL
+            if "validation_mode" in msg:
+                valmode = ["training", "validation"][msg["validation_mode"]]
+            if "topic" in msg:
+                if msg["topic"] == "switch_dataset":
+                    if msg["mode"] == 1:  # CM_TRAINALL
                         ds = "ta-"
                     else:
                         ds = ""
                     AE.next()
                     MLP.next()
-            if "perf" in line:
-                if "reg" in line:
+            if "perf" in msg:
+                if "reg" in msg:
                     AE.add_pt(ds + "reg-" +
-                            valmode, line["epoch"], line["reg"])
-                if "rec" in line:
+                            valmode, msg["epoch"], msg["reg"])
+                if "rec" in msg:
                     AE.add_pt(ds + "rec-" +
-                            valmode, line["epoch"], line["rec"])
-                AE.add_pt(ds + "total-" + valmode, line["epoch"], line["perf"])
-            if "cerr" in line:
-                MLP.add_pt(valmode, line["epoch"], line["cerr"])
-            if "topic" in line and line["topic"] == "layer_change":
+                            valmode, msg["epoch"], msg["rec"])
+                AE.add_pt(ds + "total-" + valmode, msg["epoch"], msg["perf"])
+            if "cerr" in msg:
+                MLP.add_pt(valmode, msg["epoch"], msg["cerr"])
+            if "topic" in msg and msg["topic"] == "layer_change":
                 AE.next()
                 MLP.next()
                 layer += 1
-            if "test_perf" in line:
-                print "test performance: ", line["test_perf"]
+            if "test_perf" in msg:
+                print "test performance: ", msg["test_perf"]
         self.AE = AE
         self.MLP = MLP
 
@@ -251,23 +276,25 @@ class experiment:
         pass
 
     def exptype(self):
-        if len(self.conf["stack"]) == 1:
-            if self.conf["pretrain"] == False:
-                return "1 layer, no pretraining"
+        stacksize = len(self.conf["stack"])
+        l = self.conf["stack"][0]["lambda"]
+        pretrain = self.conf["pretrain"]
+        if self.conf["stack"][0]["twolayer"] == True:
+            assert stacksize % 2 == 0
+            stacksize /= 2
+            if not pretrain:
+                return "%dL non-greedy no pretraining" % stacksize
+            elif l == 0:
+                return "%dL non-greedy unregularized" % stacksize
             else:
-                return "1 layer, with pretraining"
-        if (self.conf["pretrain"]) == False:
-            return "2 layer, no pretraining"
-        if self.conf["stack"][0]["twolayer"]:
-            if self.conf["stack"][0]["lambda"] == 0:
-                return "2 layer, non-greedy pretraining unregularized"
-            else:
-                return "2 layer, non-greedy pretraining contractive"
+                return "%dL non-greedy contractive" % stacksize
         else:
-            if self.conf["stack"][0]["lambda"] == 0:
-                return "2 layer, greedy pretraining unregularized"
+            if not pretrain:
+                return "%dL greedy no pretraining" % stacksize
+            elif l == 0:
+                return "%dL greedy unregularized" % stacksize
             else:
-                return "2 layer, greedy pretraining contractive"
+                return "%dL greedy contractive" % stacksize
 
 
 def avglambda(x):
@@ -277,7 +304,9 @@ def avglambda(x):
 
 
 def test_performance(x):
-    return x.result["test_perf"]
+    if x.result["perf"] < 0.12:
+        return x.result["test_perf"]
+    return x.result["perf"]
 
 
 def val_performance(x):
@@ -301,17 +330,26 @@ db = connection.test
 #col = db.twolayer_ae_mnist_rot2.jobs
 #gfs = gridfs.GridFS(db, "twolayer_ae_cifar_fs") # CIFAR-10
 #col = db.twolayer_ae_cifar.jobs
-gfs = gridfs.GridFS(db, "twolayer_ae_natural_fs")  # natural
-col = db.twolayer_ae_natural.jobs
+#gfs = gridfs.GridFS(db, "twolayer_ae_natural_fs")  # natural
+#col = db.twolayer_ae_natural.jobs
+#gfs = gridfs.GridFS(db, "msrc")  # msrc
+#col = db.msrc.jobs
+#logs = db.msrc.log
+gfs = gridfs.GridFS(db, "deep_mnist_rot4_fs")  # msrc
+col = db.deep_mnist_rot4.jobs
+logs = db.deep_mnist_rot4.log
 #gfs = gridfs.GridFS(db, "dev_fs")
 #col = db.dev.jobs
 
 
 experiments = []
-for x in col.find({"state": 2}):
+logs.create_index("nr")
+for x in col.find({"state": 2}).sort("ftime"):
     #if x["payload"]["conf"]["bs"]!=16:
     #    continue
     #if x["payload"]["conf"]["stack"][0]["size"]!=1000:
+    #    continue
+    #if "opt" not in x:
     #    continue
 
     x = experiment(x)
@@ -325,60 +363,128 @@ for x in col.find({"state": 2}):
     #x.parse_log(show_weights=True); plt.show()
     #if len(x.conf["stack"]) != 2: continue
     #if x.conf["stack"][0]["size"] != 512: continue
-    #if x.conf["stack"][1]["size"] != 256: continue
+    if x.conf["dataset"] != "mnist_rot":
+        continue
+    if val_performance(x) <= 0:
+        continue
     experiments.append(x)
+print "Total number of experiments: ", len(experiments)
 
-import pdb
-pdb.set_trace()
+#import pdb
+#pdb.set_trace()
 
 assert len(experiments) > 0
 
-fig = plt.figure()
+fig = plt.figure(figsize=(12, 12))
+fig.subplots_adjust(bottom=0.00, top=0.95, left=0.00,
+        right=1.00, hspace=0.05, wspace=0.10)
 types = np.unique(x.exptype() for x in experiments).tolist()
 cmapv, mmapv = {}, {}
 for i, t in enumerate(types):
-    cmapv[t] = ["y", "c", "r", "b", "m", "g", "k"][i]
+    #cmapv[t] = ["y", "c", "r", "b", "m", "g", "k"][i]
+    cmapv[t] = cm.Paired(float(i) / len(types))
 for i, t in enumerate(types):
-    mmapv[t] = ["s", "o", "^", "d", "+", "x", "p"][i]
-cmap = lambda x: cmapv[x]
+    mmapv[t] = [
+            "s", "o", "^", "v", "d",
+            "s", "o", "^", "v", "d",
+            "s", "o", "^"][i]
+
+# it may be interpreted as an index to the color map
+cmap = lambda x: rgb2hex(cmapv[x])
 mmap = lambda x: mmapv[x]
 #cmap = lambda x: plt.cm.jet(types.index(x)/float(len(types)))
 
 from scipy.stats.mstats import mquantiles
-Q = mquantiles(map(test_performance, experiments), prob=(0, 0.75))
+Q = mquantiles(map(test_performance, experiments), prob=(0, 0.60))
 y0 = Q[0]
 y1 = Q[1]
+Q = mquantiles(map(val_performance, experiments), prob=(0, 0.60))
+y0 = min(y0, Q[0])
+y1 = max(y1, Q[1])
 
+y_limits = (y0, y1)
 types = sorted(types)
 
-ax = fig.add_subplot(231)
-for t in types:
-    if "unregularized" in t:
-        continue
-    if "no pretraining" in t:
-        continue
-    L = filter(lambda x: x.exptype() == t, experiments)
-    X = map(lambda x: avglambda(x), L)
-    X = np.array(X)
-    X[X == 0] = np.min(X[X > 0]) / 10.
-    Y = map(performance, L)
-    ax.scatter(X, Y, label=t, c=cmap(t), marker=mmap(t))
-    ax.set_title("lambda")
-ax.set_xscale('log')
-ax.set_ylim(y0, y1)
 
-ax = fig.add_subplot(232)
-for t in types:
-    if "no pretraining" in t:
-        continue
-    L = filter(lambda x: x.exptype() == t, experiments)
-    X = map(lambda x: x.conf["stack"][0]["lr"], L)
-    Y = map(performance, L)
-    ax.set_title("autoencoder learnrate")
-    ax.scatter(X, Y, label=t, c=cmap(t), marker=mmap(t))
-ax.set_ylim(y0, y1)
+def plot_param(title, ax, f, perf, experiments, types,
+        y_limits=y_limits, log=False):
+    max_x = -np.infty
+    min_x = np.infty
+    for t in types:
+        try:
+            L = filter(lambda x: x.exptype() == t, experiments)
+            print "Plotting ", title, " for ", t
+            X = map(f, L)
+            X = filter(lambda x: x, X)
+            if len(X) == 0:
+                continue
+            L = [l for x, l in zip(X, L) if x != None]
+            Y = map(perf, L)
+            max_x = max(max_x, max(X))
+            min_x = min(min_x, min(X))
+            Lopt = np.where(map(lambda x: x.opt, L))
+            if np.sum(Lopt) > 0:
+                ax.scatter(np.asarray(X)[Lopt], np.asarray(Y)[Lopt],
+                            c="#FFFFFF", marker='o', s=200)
+            ax.scatter(X, Y, label=t, c=cmap(t), marker=mmap(t), s=50)
+        except RuntimeError as e:
+            print "Plotting ", title, " for ", t, " aborted: ", repr(e)
+            continue
+    if log:
+        ax.set_xscale('log')
+    ax.set_title(title)
+    ax.set_xlim(min_x, max_x)
+    ax.set_ylim(y_limits)
 
-ax = fig.add_subplot(233)
+
+def extract_layer_size(x, i):
+    if x.conf["stack"][0]["twolayer"]:
+        i = i * 2 + 1
+    if len(x.conf["stack"]) <= i:
+        return None
+    return x.conf["stack"][i]["size"]
+
+
+def extract_lambda(x):
+    if "unregularized" in x.exptype():
+        raise RuntimeError()
+    if "no pretraining" in x.exptype():
+        raise RuntimeError()
+    return np.mean([y["lambda"] for y in x.conf["stack"]])
+
+ax = fig.add_subplot(331)
+plot_param("lambda", ax, extract_lambda, val_performance, experiments, types)
+
+ax = fig.add_subplot(332)
+plot_param("aes_lr", ax, lambda x: x.conf["stack"][0]["lr"],
+        val_performance, experiments, types)
+
+ax = fig.add_subplot(333)
+plot_param("mlp_lr", ax, lambda x: x.conf["mlp_lr"],
+        val_performance, experiments, types)
+ax.legend()
+
+ax = fig.add_subplot(334)
+plot_param("s[0]", ax, lambda x: extract_layer_size(x, 0),
+        val_performance, experiments, types)
+
+ax = fig.add_subplot(335)
+plot_param("bs", ax, lambda x: x.conf["bs"],
+        val_performance, experiments, types)
+
+ax = fig.add_subplot(336)
+plot_param("finetune_bs", ax, lambda x: x.conf["finetune_bs"],
+        val_performance, experiments, types)
+
+ax = fig.add_subplot(337)
+plot_param("s[1]", ax, lambda x: extract_layer_size(x, 1),
+        val_performance, experiments, types)
+
+fig.savefig("params.png")
+fig.savefig("params.pdf")
+
+overall_fig = plt.figure(figsize=(8, 4))
+ax = overall_fig.add_subplot(121)
 for t in types:
     L = filter(lambda x: x.exptype() == t, experiments)
     L = filter(lambda x: performance(x) < 1.0, L)
@@ -389,26 +495,18 @@ for t in types:
     Y = map(performance, L)
     if not len(X):
         continue
-    ax.set_title("Exptype")
     idxbest = np.argmin(Y)
     best = L[idxbest]
     ax.scatter([X[idxbest]], [test_performance(best)],
-            c=cmap(t), marker=mmap(t), s=100)
-    ax.scatter(X, Y, label=t, marker=mmap(t), c=cmap(t))
+            c=cmap(t), marker=mmap(t), s=100, label=t)
+    ax.scatter(X, Y, marker=mmap(t), c=cmap(t))
+ax.set_title("Exptype")
 ax.set_ylim(y0, y1)
 ax.set_xlim(-.5, len(types) + .5)
-
-ax = fig.add_subplot(234)
-for t in types:
-    L = filter(lambda x: x.exptype() == t, experiments)
-    X = map(lambda x: x.conf["mlp_lr"], L)
-    Y = map(performance, L)
-    ax.scatter(X, Y, label=t, marker=mmap(t), c=cmap(t))
-    ax.set_title("MLP learnrate")
-ax.set_ylim(y0, y1)
+ax.set_xlabel("condition")
+ax.set_ylabel("performance")
 ax.legend()
-
-ax = fig.add_subplot(235)
+ax = overall_fig.add_subplot(122)
 for t in types:
     L = filter(lambda x: x.exptype() == t, experiments)
     L = filter(lambda x: val_performance(x) < 1.0, L)
@@ -421,14 +519,23 @@ for t in types:
         continue
     idxbest = np.argmin(X)
     best = L[idxbest]
-    ax.scatter([X[idxbest]], [test_performance(best)],
+    Lopt = np.where(map(lambda x: x.opt, L))
+    if np.sum(Lopt) > 0:
+        ax.scatter(np.asarray(Y)[Lopt], np.asarray(X)[Lopt],
+                c="#FFFFFF", marker='o', s=200)
+    ax.scatter([test_performance(best)], [X[idxbest]],
             c=cmap(t), marker=mmap(t), s=100)
-    ax.scatter(X, Y, label=t, marker=mmap(t), c=cmap(t))
-    ax.set_title("Generalization")
+    ax.scatter(Y, X, label=t, marker=mmap(t), c=cmap(t))
+ax.set_title("Generalization")
 ax.set_ylim(y0, y1)
 ax.set_xlim(y0, y1)
-ax.set_xlabel("validation performance")
-ax.set_ylabel("test performance")
+ax.set_xlabel("test perf")
+ax.set_ylabel("val perf")
+overall_fig.savefig("overall.png")
+overall_fig.savefig("overall.pdf")
+os.system("gzip -f params.pdf")
+os.system("gzip -f overall.pdf")
+
 
 longest_L = 0
 for t in types:
@@ -448,8 +555,14 @@ for t in types:
         (t, test_performance(best))
     print "                           validation `%s': %2.3f" % \
         (t, val_performance(best))
-    #best.show()
-    #best.parse_log(show_weights=True);plt.show()
+    sw = "non-greedy" in best.exptype()
+    if False and sw:
+        best.load_log(logs)
+        best.parse_log(show_weights=True)
+        #best.show()
+        plt.show()
+print "overall best on valset:"
+print sorted(experiments, key=val_performance)[0].conf
 for t in types:
     L = filter(lambda x: x.exptype() == t, experiments)
     best = sorted(L, key=val_performance)[0]
@@ -458,14 +571,18 @@ for t in types:
                 (t, best.AE.average_min("rec-validation"))
     except:
         continue
-if 1:
+if 0:
     figa = plt.figure(figsize=(5.5, 4))
     ax = figa.add_subplot(111)
     mytypes = [
         "1 layer, with pretraining",
         "2 layer, greedy pretraining unregularized",
         "2 layer, greedy pretraining contractive",
-        "2 layer, non-greedy pretraining contractive"]
+        "2 layer, non-greedy pretraining contractive",
+        "3 layer, greedy pretraining unregularized",
+        "3 layer, greedy pretraining contractive",
+        "3 layer, non-greedy pretraining contractive",
+        ]
     for i, t in enumerate(mytypes):
         perms = []
         L = filter(lambda x: x.exptype() == t, experiments)
@@ -502,7 +619,7 @@ if 1:
             ax.set_xlim(min(vals), 1 * max(vals))
             ax.set_xlabel("validation error")
             ax.set_ylabel("fraction below")
-            #ax.legend(loc='lower right',prop=smallFont)
+            ax.legend(loc='lower right', prop=smallFont)
             ax.set_title("MNIST Dataset")
 
         if 0:
@@ -571,8 +688,7 @@ if 0:
     ax.legend()
     figb.savefig("recerr.pdf")
 
-fig.savefig("perf.pdf")
-plt.show()
+#plt.show()
 import sys
 sys.exit(0)
 
