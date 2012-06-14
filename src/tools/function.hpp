369baaf2c060552dd1b70a1bedb3dc9b34ee3109
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-06-14 21:05:07 +0200

    default constructor and reset() for functions

--- src/tools/function.hpp
+++ src/tools/function.hpp
@@ -12,16 +12,21 @@ namespace cuvnet
         private:
             boost::shared_ptr<Op>   m_op;
             boost::shared_ptr<Sink> m_sink;
-            swiper                  m_swiper;
+            std::auto_ptr<swiper>   m_swiper;
         public:
-            function(boost::shared_ptr<Op> op, int result=0, const std::string& name="")
-                : m_op(op)
-                , m_sink(boost::make_shared<Sink>(name, m_op->result(result)))
-                , m_swiper(*m_op, result, std::vector<Op*>()){
-                }
+            function(){}
+            function(boost::shared_ptr<Op> op, int result=0, const std::string& name=""){
+                reset(op,result,name);
+            }
+
+            void reset(boost::shared_ptr<Op> op, int result=0, const std::string& name=""){
+                m_op   = op;
+                m_sink = boost::make_shared<Sink>(name, op->result(result));
+                m_swiper.reset(new swiper(*m_op, result, std::vector<Op*>()));
+            }
             
             const matrix& evaluate(){
-                m_swiper.fprop();
+                m_swiper->fprop();
                 return m_sink->cdata();
             }
             const matrix& result()const{
