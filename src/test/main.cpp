a088f1745e14104b80bc323565df8ad9e8bdcd06
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-06-18 13:49:52 +0200

    move device init to test main()

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,10 +1,26 @@
 //#include <glog/logging.h>
+#include <cuv.hpp>
+#include <cuvnet/common.hpp>
 #include <gtest/gtest.h>
 
+class FooEnvironment 
+: public ::testing::Environment
+{
+ public:
+  virtual void SetUp() {
+     cuv::initCUDA(0);
+      if(cuv::IsSame<cuv::dev_memory_space, cuvnet::matrix::memory_space_type>::Result::value){
+          cuv::initialize_mersenne_twister_seeds();
+      }
+  }
+  virtual void TearDown() {}
+};
 
 int main(int argc, char **argv) {
   //google::InitGoogleLogging(argv[0]);
   ::testing::InitGoogleTest(&argc, argv);
+  ::testing::Environment* const foo_env 
+      = ::testing::AddGlobalTestEnvironment(new FooEnvironment);
   return RUN_ALL_TESTS();
 }
 
