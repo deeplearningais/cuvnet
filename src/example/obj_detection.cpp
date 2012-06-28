7d8a84ef0fc47b53dae649b77e6fb67fc330f3b3
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-06-28 18:14:56 +0200

    load batch before dropping to python

--- src/example/obj_detection.cpp
+++ src/example/obj_detection.cpp
@@ -78,6 +78,7 @@ int main(int argc, char **argv)
     std::vector<Op*> params = od->params();
 
     {
+        load_batch(input, ignore, target, &ds, bs);
         try{
             initialize_python();
             export_ops();
