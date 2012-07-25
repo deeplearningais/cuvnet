03e432211a6eb5c6fe3a002778d913b906f95f3e
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-07-25 19:11:05 +0200

    dbg.sh: also show bprop step

--- tools/dbg.sh
+++ tools/dbg.sh
@@ -2,4 +2,4 @@
 
 for i in dbg/bprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
 for i in dbg/fprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
-geeqie dbg/fprop-0*/*.png dbg/fprop-0*/*.png
+geeqie dbg/fprop-0*/*.png dbg/bprop-0*/*.png
