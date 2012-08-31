e00984a21c93633ca51797e1e808b70a49f0b824
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-08-31 16:20:11 +0200

    added 2-layer auto-encoder as well as contractive auto encoders (1L and 2L)

---
+++ src/cuvnet/models/contractive_auto_encoder.hpp
@@ -0,0 +1,125 @@
+#ifndef __CONTRACTIVE_AUTO_ENCODER_HPP__
+#     define __CONTRACTIVE_AUTO_ENCODER_HPP__
+
+#include <boost/tuple/tuple.hpp>
+#include "simple_auto_encoder.hpp"
+#include "regularizers.hpp"
+
+namespace cuvnet
+{
+/**
+ * This is a contractive auto-encoder.
+ *
+ * @ingroup models
+ */
+class contractive_auto_encoder 
+: public simple_auto_encoder
+{
+    /// how much regularization is required
+    float m_reg;
+
+    public:
+    typedef simple_auto_encoder::op_ptr op_ptr;
+
+    /**
+     * constructor
+     * 
+     * @param hidden_dim the number of dimensions of the hidden layer
+     * @param binary if true, assumes inputs are bernoulli distributed
+     * @param reg regularization strength
+     */
+    contractive_auto_encoder(unsigned int hidden_dim, bool binary, float reg)
+    :simple_auto_encoder(hidden_dim,binary)
+    ,m_reg(reg)
+    {
+    }
+
+    /**
+     * Returns the L2 regularization loss.
+     */
+    virtual boost::tuple<float,op_ptr> regularize(){
+        return boost::make_tuple(
+                m_reg,
+                sum(  sum_to_vec(pow(m_encoded*(1.f-m_encoded),2.f),1)
+                    * sum_to_vec(pow(m_weights,2.f),1))
+                );
+    }
+};
+
+/**
+ * This is a contractive two-layer auto-encoder.
+ *
+ * @ingroup models
+ */
+class two_layer_contractive_auto_encoder 
+: public two_layer_auto_encoder
+{
+    /// how much regularization is required
+    float m_reg;
+
+    /// stored so we can make it non-stochastic for testing
+    boost::shared_ptr<RowSelector> m_rs;
+
+    public:
+    typedef two_layer_auto_encoder::op_ptr op_ptr;
+
+    /**
+     * turn stochastic row selection on/off.
+     *
+     * Mainly useful for derivative testing.
+     */
+    void set_stochastic(bool b){ m_rs->set_random(b); }
+
+    /**
+     * constructor
+     * 
+     * @param input the function that generates the input of the autoencoder
+     * @param hidden_dim0 the number of dimensions of the 1st hidden layer
+     * @param hidden_dim1 the number of dimensions of the 2nd hidden layer
+     * @param binary if true, assumes inputs are bernoulli distributed
+     * @param reg regularization strength
+     */
+    two_layer_contractive_auto_encoder(unsigned int hidden_dim0, unsigned int hidden_dim1, bool binary, float reg)
+    :two_layer_auto_encoder(hidden_dim0, hidden_dim1,binary)
+    ,m_reg(reg)
+    {
+    }
+
+    /**
+     * Returns the L2 regularization loss.
+     */
+    virtual boost::tuple<float,op_ptr> regularize(){
+        op_ptr contractive_loss;
+        unsigned int bs = m_input->result()->shape[0];
+        //unsigned int num_contr = std::max(1u,bs/16);
+        unsigned int num_contr = 1;
+        for(unsigned int i=0;i<num_contr;i++){
+            m_rs  = row_select(m_hl0,m_hl1); // select same (random) row in m_hl0 and m_hl1
+            op_ptr h0r = result(m_rs,0);
+            op_ptr h1r = result(m_rs,1);
+
+            //op_ptr h1_ = (1.f-pow(h1r,2.f));// tanh
+            op_ptr h0_ = h0r*(1.f-h0r); // logistic
+            op_ptr h1_ = h1r*(1.f-h1r); // logistic
+
+            op_ptr tmp = sum( sum_to_vec(pow(prod(mat_times_vec(m_weights0,h0_,1), m_weights1),2.f),1)*pow(h1_,2.f));
+            if(i == 0) 
+                contractive_loss = tmp;
+            else if(i == 1)
+                contractive_loss = tmp + contractive_loss;
+            else
+                contractive_loss = add_to_param(contractive_loss,tmp);
+
+        }
+        //op_ptr J      = mat_times_vec(prod(mat_times_vec(m_weights1,h2_,1), m_weights2),h1_,0);
+        //contractive_loss = sum( pow(J, 2.f) );
+
+        return boost::make_tuple( 
+                (m_reg * bs) / num_contr, contractive_loss);
+    }
+};
+
+}
+
+
+#endif /* __CONTRACTIVE_AUTO_ENCODER_HPP__ */
--- src/cuvnet/models/simple_auto_encoder.hpp
+++ src/cuvnet/models/simple_auto_encoder.hpp
@@ -104,6 +104,129 @@ class simple_auto_encoder
     }
 };
 
+/**
+ * This is a two-layer symmetric auto-encoder.
+ *
+ * @ingroup models
+ */
+class two_layer_auto_encoder 
+: public generic_auto_encoder
+{
+    public:
+        typedef boost::shared_ptr<Op>     op_ptr;
+
+    protected:
+        // these are the parameters of the model
+        boost::shared_ptr<ParameterInput>  m_weights0; ///< weights from input to 1st hidden layer
+        boost::shared_ptr<ParameterInput>  m_weights1; ///< weights from input to 2nd hidden layer
+        boost::shared_ptr<ParameterInput>  m_bias_h0;  ///< bias of 1st hidden layer
+        boost::shared_ptr<ParameterInput>  m_bias_h1;  ///< bias of 2nd hidden layer
+        boost::shared_ptr<ParameterInput>  m_bias_h0_;  ///< bias of 2nd hidden layer (decoding)
+        boost::shared_ptr<ParameterInput>  m_bias_y;   ///< bias of output layer
+
+        unsigned int m_hidden_dim0;  ///< size of 1st hidden layer
+        unsigned int m_hidden_dim1;  ///< size of 2nd hidden layer
+
+        op_ptr m_hl0; ///< the 1st hidden layer activation
+        op_ptr m_hl1; ///< the 2nd hidden layer activation
+
+    public:
+
+        /// \f$ h := \sigma ( x W + b_h ) \f$
+        virtual op_ptr  encode(op_ptr& inp){
+            if(!m_weights0){
+                inp->visit(determine_shapes_visitor()); 
+                unsigned int input_dim = inp->result()->shape[1];
+
+                m_weights0.reset(new ParameterInput(cuv::extents[input_dim][m_hidden_dim0],"weights0"));
+                m_weights1.reset(new ParameterInput(cuv::extents[m_hidden_dim0][m_hidden_dim1],"weights1"));
+                m_bias_h0.reset(new ParameterInput(cuv::extents[m_hidden_dim0],            "bias_h0"));
+                m_bias_h0_.reset(new ParameterInput(cuv::extents[m_hidden_dim0],            "bias_h0_"));
+                m_bias_h1 .reset(new ParameterInput(cuv::extents[m_hidden_dim1],            "bias_h1"));
+                m_bias_y  .reset(new ParameterInput(cuv::extents[input_dim],                "bias_y"));
+            }else{
+                inp->visit(determine_shapes_visitor()); 
+                unsigned int input_dim = inp->result()->shape[1];
+                cuvAssert(m_weights0->data().shape(0)==input_dim);
+            }
+
+            m_hl0 = logistic(mat_plus_vec(prod(inp,   m_weights0),m_bias_h0,1));
+            m_hl1 = logistic(mat_plus_vec(prod(m_hl0, m_weights1),m_bias_h1,1));
+            return m_hl1;
+        }
+        /// decode the encoded value given in `enc`
+        virtual op_ptr  decode(op_ptr& enc){ 
+            op_ptr h0_ = logistic(mat_plus_vec(prod(enc, m_weights1, 'n','t'), m_bias_h0_, 1));
+            op_ptr y_  =          mat_plus_vec(prod(h0_, m_weights0, 'n','t'), m_bias_y, 1);
+            return y_;
+        }
+
+        /**
+         * Determine the parameters learned during pre-training
+         * @overload
+         */
+        virtual std::vector<Op*> unsupervised_params(){
+            using namespace boost::assign;
+            std::vector<Op*> v;
+            v += m_weights0.get(), m_weights1.get(), m_bias_h0.get(), m_bias_h1.get(), m_bias_h0_.get(), m_bias_y.get();
+            return v;
+        };
+
+        /**
+         * Determine the parameters learned during fine-tuning
+         * @overload
+         */
+        virtual std::vector<Op*> supervised_params(){
+            using namespace boost::assign;
+            std::vector<Op*> v;
+            v += m_weights0.get(), m_weights1.get(), m_bias_h0.get(), m_bias_h1.get();
+            return v;
+        };
+
+        /**
+         * constructor
+         * 
+         * @param input the function that generates the input of the autoencoder
+         * @param hidden_dim0 the number of dimensions of the 1st hidden layer
+         * @param hidden_dim1 the number of dimensions of the 2nd hidden layer
+         * @param binary if true, assumes inputs are bernoulli distributed
+         */
+        two_layer_auto_encoder(unsigned int hidden_dim0, unsigned int hidden_dim1, bool binary)
+            :generic_auto_encoder(binary)
+             ,m_hidden_dim0(hidden_dim0)
+             ,m_hidden_dim1(hidden_dim1)
+    {
+    }
+
+        /**
+         * initialize the weights with random numbers
+         * @overload
+         */
+        virtual void reset_weights(){
+            {
+                unsigned int input_dim = m_weights0->data().shape(0);
+                float diff = 4.f*std::sqrt(6.f/(input_dim+m_hidden_dim0));
+                cuv::fill_rnd_uniform(m_weights0->data());
+                m_weights0->data() *= 2*diff;
+                m_weights0->data() -=   diff;
+            }
+            {
+                unsigned int input_dim = m_weights1->data().shape(0);
+                float diff = 4.f*std::sqrt(6.f/(input_dim+m_hidden_dim1));
+                cuv::fill_rnd_uniform(m_weights1->data());
+                m_weights1->data() *= 2*diff;
+                m_weights1->data() -=   diff;
+            }
+            m_bias_h0->data()   = 0.f;
+            m_bias_h1->data()   = 0.f;
+            m_bias_h0_->data()  = 0.f;
+            m_bias_y->data()   = 0.f;
+        }
+};
+
+/**
+ * L2-regularized simple auto-encoder.
+ */
 struct l2reg_simple_auto_encoder
 : public simple_auto_encoder
 , public simple_weight_decay
