c493b5703f7c2a3441b65aca3329b321d5af16cb
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-10-01 13:14:19 +0200

    only add non-zero losses to monitor

--- src/tools/monitor.cpp
+++ src/tools/monitor.cpp
@@ -80,8 +80,10 @@ namespace cuvnet
         }
     }
     monitor& monitor::add(watchpoint_type type, boost::shared_ptr<Op> op, const std::string& name, unsigned int result){
-        m_impl->m_watchpoints.push_back(new watchpoint(type,op,name,result));
-        m_impl->m_wpmap[name] = m_impl->m_watchpoints.back();
+        if(op){
+            m_impl->m_watchpoints.push_back(new watchpoint(type,op,name,result));
+            m_impl->m_wpmap[name] = m_impl->m_watchpoints.back();
+        }
         return *this;
     }
     void monitor::set_training_phase(cv_mode mode, int split){
