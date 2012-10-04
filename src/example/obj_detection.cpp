5d5e56234eb32838337456a33cff93b718910c53
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-10-04 20:38:18 +0200

    remove BioID stuff for now

--- src/example/obj_detection.cpp
+++ src/example/obj_detection.cpp
@@ -21,8 +21,6 @@ using namespace boost::assign;
 using namespace cuvnet;
 namespace ll = boost::lambda;
 
-#define USE_BIOID 0
-
 /**
  * load a batch from the dataset
  */
@@ -43,8 +41,6 @@ struct voc_loader{
             boost::shared_ptr<ParameterInput> target,
             voc_detection_dataset* ds,
             unsigned int bs){
-        std::cout << "." << std::flush;
-
         L.clear();
         ds->get_batch(L, bs);
 
@@ -58,37 +54,10 @@ struct voc_loader{
     }
 };
 
-/**
- * load a batch from the dataset
- */
-void load_batch_bioid(
-        boost::shared_ptr<ParameterInput> input,
-        boost::shared_ptr<ParameterInput> ignore,
-        boost::shared_ptr<ParameterInput> target,
-        cuv::tensor<float,cuv::host_memory_space>* data,
-        cuv::tensor<float,cuv::host_memory_space>* labels,
-        unsigned int bs, unsigned int batch){
-    input->data() = (*data)[cuv::indices[cuv::index_range(batch*bs,(batch+1)*bs)][cuv::index_range()]];
-    target->data() = (*labels)[cuv::indices[cuv::index_range(batch*bs,(batch+1)*bs)][cuv::index_range()]];
-    unsigned int imgX = sqrt(data->shape(1));
-    unsigned int tgtX = sqrt(labels->shape(1)/2);
-    input->data() .reshape(cuv::extents[bs][1][imgX][imgX]);
-    target->data().reshape(cuv::extents[bs][2][tgtX][tgtX]);
-    ignore->data() = 1.f;
-}
-
-static unsigned int cnt=0;
-void save_weights(obj_detector* ds, unsigned int epoch){
-    if(cnt++ % 100 == 0){
-        tofile((boost::format("conv1-%05d.npy") % cnt ).str(), ds->m_conv1_weights->data());
-        tofile((boost::format("conv2-%05d.npy") % cnt ).str(), ds->m_conv2_weights->data());
-    }
-}
-
 int main(int argc, char **argv)
 {
     // initialize cuv library   
-    cuv::initCUDA(1);
+    cuv::initCUDA(0);
     cuv::initialize_mersenne_twister_seeds();
     cuvnet::Logger log;
     std::string serialization_file = "obj_detector-%05d.ser";
@@ -96,31 +65,20 @@ int main(int argc, char **argv)
     // number of simultaneously processed items
     unsigned int bs=32;
 
-#if USE_BIOID
-    bioid_dataset ds;
-    boost::shared_ptr<ParameterInput> input(
-            new ParameterInput(cuv::extents[bs][1][120][120],"input"));
-    boost::shared_ptr<ParameterInput> ignore(
-            new ParameterInput(cuv::extents[bs][2][30][30],"ignore"));
-    boost::shared_ptr<ParameterInput> target(
-            new ParameterInput(cuv::extents[bs][2][30][30],"target"));
-#else
-    voc_detection_dataset ds("/home/local/datasets/VOC2011/voc_detection_train_small.txt", "/home/local/datasets/VOC2011/voc_detection_val.txt", true);
+    //voc_detection_dataset ds("/home/local/datasets/VOC2011/voc_detection_train_small.txt", "/home/local/datasets/VOC2011/voc_detection_val.txt", true);
+    voc_detection_dataset ds("/home/local/datasets/VOC2011/voc_detection_train.txt", "/home/local/datasets/VOC2011/voc_detection_val.txt", true);
     boost::shared_ptr<ParameterInput> input(
             new ParameterInput(cuv::extents[bs][3][128][128],"input"));
-    boost::shared_ptr<ParameterInput> ignore(
-            new ParameterInput(cuv::extents[bs][1][128][128],"ignore"));
-    boost::shared_ptr<ParameterInput> target(
-            new ParameterInput(cuv::extents[bs][1][128][128],"target"));
-#endif
+    boost::shared_ptr<ParameterInput> ignore;
+    boost::shared_ptr<ParameterInput> target;
 
     boost::shared_ptr<obj_detector> od;
 
     bool load_old_model_and_drop_to_python = argc > 1;
     if(! load_old_model_and_drop_to_python){
         // create new network
-        od.reset( new obj_detector(7,32,7,32) );
-        od->init(input,ignore,target);
+        od.reset( new obj_detector(5,32,7,64) );
+        od->init(input, 1);
     }else{
         //bs = 1;
         input.reset();
@@ -128,29 +86,24 @@ int main(int argc, char **argv)
         target.reset();
         od = deserialize_from_file<obj_detector>(serialization_file, boost::lexical_cast<int>(argv[1]));
         input  = boost::dynamic_pointer_cast<ParameterInput>(get_node<ParameterInput>(od->get_loss(), "input")->shared_from_this());
-        target = boost::dynamic_pointer_cast<ParameterInput>(get_node<ParameterInput>(od->get_loss(), "target")->shared_from_this());
-        ignore = boost::dynamic_pointer_cast<ParameterInput>(get_node<ParameterInput>(od->get_loss(), "ignore")->shared_from_this());
     }
+    target = boost::dynamic_pointer_cast<ParameterInput>(get_node<ParameterInput>(od->get_loss(), "target")->shared_from_this());
+    ignore = boost::dynamic_pointer_cast<ParameterInput>(get_node<ParameterInput>(od->get_loss(), "ignore")->shared_from_this());
+
+    valid_shape_info vsi(input, od->hl3);
+    ds.set_output_properties(vsi.scale_h, vsi.scale_w, vsi.crop_h, vsi.crop_w);
 
 
     std::vector<Op*> params = od->params();
     if(load_old_model_and_drop_to_python){
-        //load_batch(input, ignore, target, &ds, bs);
         initialize_python();
         export_ops();
         export_op("loss", od->get_loss());
-#if USE_BIOID
-        export_loadbatch(
-                //boost::bind(load_batch_bioid,input, ignore, target,&ds.train_data, &ds.train_labels, bs, _1),
-                //ds.train_data.shape(0)/ll::constant(bs));
-                boost::bind(load_batch_bioid,input, ignore, target,&ds.test_data, &ds.test_labels, bs, _1),
-                ds.test_data.shape(0)/ll::constant(bs));
-#else
         voc_loader vl;
+        vl.load_batch(input, ignore, target, &ds, bs);
         export_loadbatch(
                 boost::bind(&voc_loader::load_batch,&vl, input, ignore, target, &ds, bs),
                 boost::bind(&voc_detection_dataset::trainset_size, &ds));
-#endif
         embed_python();
         return 0;
     }
@@ -167,8 +120,8 @@ int main(int argc, char **argv)
     std::cout << std::endl << " Training phase: " << std::endl;
     {
         // create a \c gradient_descent object 
-        rprop_gradient_descent gd(od->get_loss(),0,params);
-        //momentum_gradient_descent gd(od->get_loss(),0,params, 0.001f);
+        //rprop_gradient_descent gd(od->get_loss(),0,params);
+        momentum_gradient_descent gd(od->get_loss(),0,params, 0.01f);
         convergence_checker cs(gd, boost::bind(&monitor::mean, &mon, "total loss"));
         cs.decrease_lr(10000, .95f);
         
@@ -176,38 +129,28 @@ int main(int argc, char **argv)
         mon.register_gd(gd);
 
         // before each batch, load data into \c input
-#if USE_BIOID
-        gd.before_batch.connect(boost::bind(load_batch_bioid,input, ignore, target,&ds.train_data, &ds.train_labels, bs, _2));
-#else
         voc_loader vl;
         gd.before_batch.connect(boost::bind(&voc_loader::load_batch, &vl,input, ignore, target,&ds, bs));
         gd.after_batch.connect(boost::bind(&voc_loader::done, &vl, od->m_output, &ds));
-#endif
 
         gd.after_batch.connect(
                 boost::bind(&monitor::simple_logging, &mon));
         gd.after_epoch.connect(std::cout << ll::constant("\n"));
 
-#if !USE_BIOID
-        gd.before_epoch.connect(boost::bind(serialize_to_file<obj_detector>, serialization_file, od, _2, 100));
-#else
-        gd.after_epoch.connect(boost::bind(serialize_to_file<obj_detector>, serialization_file, od, _2, 100));
-#endif
-        //gd.before_batch.connect(boost::bind(save_weights, od.get(), _2));
+        serialize_to_file<obj_detector>(serialization_file, od, 0, 1);
+        gd.after_weight_update.connect(boost::bind(serialize_to_file<obj_detector>, serialization_file, od, _1, 640));
+        gd.after_weight_update.connect(boost::bind(&obj_detector::project_to_allowed_region, &*od));
         
         // the number of batches 
-#if USE_BIOID
-        gd.current_batch_num.connect(ds.train_data.shape(0)/ll::constant(bs));
-#else
-        gd.current_batch_num = boost::bind(&voc_detection_dataset::trainset_size, &ds);
-#endif
+        //gd.current_batch_num = boost::bind(&voc_detection_dataset::trainset_size, &ds);
+        gd.current_batch_num = ll::constant(85);
         //gd.current_batch_num.connect(ll::constant(128));
         
-        //gd.minibatch_learning(1E6, 10000*60,1,false);
-        gd.minibatch_learning(1, 10000*60,8,false);
-        gd.minibatch_learning(2, 10000*60,16,false);
-        gd.minibatch_learning(4, 10000*60,32,false);
-        gd.minibatch_learning(1E6, 10000*60,64,false);
+        gd.minibatch_learning(1E6, 10000*60,1,false);
+        //gd.minibatch_learning(1, 10000*60,8,false);
+        //gd.minibatch_learning(2, 10000*60,16,false);
+        //gd.minibatch_learning(4, 10000*60,32,false);
+        //gd.minibatch_learning(1E6, 10000*60,64,false);
         //load_batch(input,ignore,target,&ds, bs);
         //gd.batch_learning(1000, 10000*60);
     }
