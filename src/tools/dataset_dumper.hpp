6666f9519d6adac748bf5a96b12b18952eaf187f
Nenad Biresev <nbiresev@gmail.com>
2012-10-10 16:49:08 +0200

    dumper is now accumulating data and storing at the end

--- src/tools/dataset_dumper.hpp
+++ src/tools/dataset_dumper.hpp
@@ -16,72 +16,54 @@
 
 namespace cuvnet
 {
-
-    struct  dataset_reader: public dataset{
-        
-        typedef cuv::tensor<float,cuv::host_memory_space> tensor_type;
-        private:
-        std::string m_file_train; 
-        std::string m_file_test; 
-
-        public:
-
-        /**
-         * constructor
-         * 
-         */
-        dataset_reader(std::string file_name_train, std::string file_name_test):
-            m_file_train(file_name_train),
-            m_file_test(file_name_test)
-        {
-        }
-
-        void read_from_file(){
-            read(train_data, train_labels, m_file_train);
-            read(test_data, test_labels, m_file_test);
-        }
-        
-        // reads data and labels from file
-        void read(tensor_type& data, tensor_type& labels,const std::string file_name){
-            using namespace cuv;
-            
-            std::ifstream readfile(file_name.c_str());
-            boost::archive::binary_iarchive oa_read(readfile);
-
-            oa_read >> data;
-            oa_read >> labels;
-
-            readfile.close();
-        }
-    };
-
     class dataset_dumper{
         typedef cuv::tensor<float,cuv::host_memory_space> tensor_type;
+        
 
         private:
         /// file where we write the hidden activations and parameters
         std::ofstream m_logfile;
-
-
         boost::archive::binary_oarchive m_oa_log;
         std::string m_file; 
+        int m_num_batches;
+        int m_bs;
+        int m_data_dim_2;
+        int m_label_dim_2;
+        int m_current_batch_num;
+        tensor_type m_whole_data;
+        tensor_type m_whole_labels;
+
         public:
 
         /**
          * constructor
          * 
          */
-        dataset_dumper(std::string file_name, unsigned int num_batches):
+        dataset_dumper(std::string file_name, int num_batches, int bs, int data_dim_2, int label_dim_2):
             m_logfile(file_name.c_str()),
             m_oa_log(m_logfile),
-            m_file(file_name)
+            m_file(file_name),
+            m_bs(bs),
+            m_data_dim_2(data_dim_2),
+            m_label_dim_2(label_dim_2),
+            m_current_batch_num(0),
+            m_whole_data(cuv::extents[num_batches * bs][data_dim_2]),
+            m_whole_labels(cuv::extents[num_batches * bs][label_dim_2])
         {
+            
         }
 
         
         void write_to_file(const tensor_type& data, const tensor_type& labels){
-           m_oa_log << data; 
-           m_oa_log << labels;
+           //m_whole_data[cuv::indices[cuv::index_range(m_current_batch_num * m_bs, (m_current_batch_num+1) * m_bs)][cuv::index_range()]]= data;
+           //m_whole_labels[cuv::indices[cuv::index_range(m_current_batch_num * m_bs,(m_current_batch_num+1) * m_bs)][cuv::index_range()]]= labels;
+           m_current_batch_num++;
+           std::cout << "batch num " << m_current_batch_num << std::endl;/* cursor */
+           // writes to the file the whole data ones it is accumulated 
+           //if(m_current_batch_num == m_num_batches){
+           //    m_oa_log << m_whole_data; 
+           //    m_oa_log << m_whole_labels;
+           //}
         }
 
 
