a0f4df227894a3e42401872001dc7c6317321def
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-01-31 20:00:34 +0100

    more examples of what to do with models in python

--- src/example/linear_regression_python.py
+++ src/example/linear_regression_python.py
@@ -1,9 +1,25 @@
 # for converting cuv tensors to numpy and back
-import cuv_python as cp
+import numpy as np
 import pylinreg
+import gtk
 
 lr = pylinreg.linear_regression(100, 20, 10)
 
-print lr.X.data.np
-print lr.Y.data.np
-print lr.W.data.np
+try:
+    import xdot
+    # Show the loss using GraphViz, xdot.py
+    # You can also react to clicks by subclassing DotWindow.
+    W = xdot.DotWindow()
+    W.set_dotcode(lr.loss.dot())
+    W.connect('destroy', gtk.main_quit)
+    gtk.main()
+except:
+    print "make sure xdot is installed!"
+    pass
+
+import cuv_python as cp
+# print some data stored in the inputs
+lr.Y.data = cp.dev_tensor_float(np.ones(lr.Y.data.shape).astype("float32"))
+lr.W.data = cp.dev_tensor_float(np.random.uniform(size=lr.W.data.shape).astype("float32"))
+print lr.Y.data.np[:5, :5]
+print lr.W.data.np[:5, :5]
