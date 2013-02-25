a0a985ff22f5503c07474115602b6b18eba92b13
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-02-25 15:16:39 +0100

    serialize m_dim member of pairwise_norm

--- src/cuvnet/ops/convolve.hpp
+++ src/cuvnet/ops/convolve.hpp
@@ -533,7 +533,7 @@ namespace cuvnet
                 friend class boost::serialization::access;
                 template<class Archive>
                     void serialize(Archive& ar, const unsigned int version){
-                        ar & boost::serialization::base_object<Op>(*this);
+                        ar & boost::serialization::base_object<Op>(*this) & m_dim;
                     }
         };
 
