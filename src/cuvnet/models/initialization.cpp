f500a4763f968df5ed3befacae7eb05ba135afee
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-05-05 22:27:43 +0200

    initialization methods for ParameterInputs used as weights

--- src/cuvnet/models/initialization.cpp
+++ src/cuvnet/models/initialization.cpp
@@ -16,5 +16,24 @@ namespace cuvnet
             p->data() *= diff*2.f;
             p->data() -= diff;
         }
+
+        void set_learnrate_factors_lecun_dense(
+                boost::shared_ptr<ParameterInput> W, unsigned int input_dim,
+                boost::shared_ptr<ParameterInput> b){
+            cuvAssert(W->data().ndim()==2);
+            cuvAssert(b->data().ndim()==1);
+            unsigned int n_inputs = W->data().shape(input_dim);
+            W->set_learnrate_factor(1.f / sqrt((float)n_inputs));
+            b->set_learnrate_factor(1.f);
+            b->set_weight_decay_factor(0.f);
+        }
+
+        void set_learnrate_factors_lecun_conv(
+                boost::shared_ptr<ParameterInput> W, unsigned int nshares,
+                boost::shared_ptr<ParameterInput> b = boost::shared_ptr<ParameterInput>()){
+            W->set_learnrate_factor(1.f / sqrt((float)nshares));
+            b->set_learnrate_factor(1.f / sqrt((float)nshares));
+            b->set_weight_decay_factor(0.f);
+        }
     }
 }
--- src/cuvnet/models/initialization.hpp
+++ src/cuvnet/models/initialization.hpp
@@ -14,6 +14,41 @@ namespace cuvnet
          * @param act_func_tanh if false, assume logistic activation function
          */
         void initialize_dense_glorot_bengio(boost::shared_ptr<ParameterInput> p, bool act_func_tanh=true);
+
+        /**
+         * Set learnrate factors as advertised by LeCun
+         *
+         * Dense case:  "Because of possible correlations between input
+         * variables, the learning rate of a unit should be inversely
+         * proportional to the square root of the number of inputs to the
+         * unit."
+         *
+         * @param W the weights for which learnrate is to be set
+         * @param bias the bias for which learnrate is to be set
+         * @param input_dim the axis of the weights whose shape reflects the dimensionality of the input
+         */
+        void set_learnrate_factors_lecun_dense(
+                boost::shared_ptr<ParameterInput> W, unsigned int input_dim=0, 
+                boost::shared_ptr<ParameterInput> bias = boost::shared_ptr<ParameterInput>());
+
+        /**
+         * Set learnrate factors as advertised by LeCun
+         *
+         * "If shared weights are used (as in TDNNs and
+         * convolutional networks), the learning rate of
+         * a weight should be inversely proportional to the
+         * square root of the number of connection sharing
+         * that weight."
+         *
+         * @param W the weights for which learnrate is to be set
+         * @param b the bias for which learnrate is to be set
+         * @param nshares the number of times one weight is shared.
+         *                this should be proportional to the output
+         *                map size.
+         */
+        void set_learnrate_factors_lecun_alexconv(
+                boost::shared_ptr<ParameterInput> W, unsigned int nshares,
+                boost::shared_ptr<ParameterInput> b = boost::shared_ptr<ParameterInput>());
     }
 }
 
