faab7eadf55b81b5b4f679933a579cd7dce1c1fb
Nenad Biresev <nbiresev@gmail.com>
2013-05-08 16:45:32 +0200

    FIX sqrt namespace problem

--- src/cuvnet/models/initialization.cpp
+++ src/cuvnet/models/initialization.cpp
@@ -1,6 +1,7 @@
 #include <cuv.hpp>
 #include <cuvnet/ops/input.hpp>
 #include "initialization.hpp"
+#include <cmath>
 
 namespace cuvnet
 {
@@ -48,7 +49,7 @@ namespace cuvnet
             cuvAssert(W->data().ndim()==2);
             cuvAssert(b->data().ndim()==1);
             unsigned int n_inputs = W->data().shape(input_dim);
-            W->set_learnrate_factor(1.f / sqrt((float)n_inputs));
+            W->set_learnrate_factor(1.f / std::sqrt((float)n_inputs));
             b->set_learnrate_factor(1.f);
             b->set_weight_decay_factor(0.f);
         }
@@ -56,8 +57,8 @@ namespace cuvnet
         void set_learnrate_factors_lecun_conv(
                 boost::shared_ptr<ParameterInput> W, unsigned int nshares,
                 boost::shared_ptr<ParameterInput> b = boost::shared_ptr<ParameterInput>()){
-            W->set_learnrate_factor(1.f / sqrt((float)nshares));
-            b->set_learnrate_factor(1.f / sqrt((float)nshares));
+            W->set_learnrate_factor(1.f / std::sqrt((float)nshares));
+            b->set_learnrate_factor(1.f / std::sqrt((float)nshares));
             b->set_weight_decay_factor(0.f);
         }
     }
