0ae62df84560ae6bf67d58f3c893cb994678c1a7
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-06-20 13:08:35 +0200

    FIX serialization of metamodel

--- src/cuvnet/models/models.hpp
+++ src/cuvnet/models/models.hpp
@@ -82,7 +82,7 @@ namespace cuvnet
                 friend class boost::serialization::access;
                 template<class Archive>
                     void serialize(Archive& ar, const unsigned int version){
-                        ar & boost::serialization::base_object<boost::enable_shared_from_this<model> >(*this);
+                        ar & boost::serialization::base_object<model>(*this);
                         ar & m_models;
                     }
             protected:
