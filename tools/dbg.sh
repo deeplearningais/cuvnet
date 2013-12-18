a5951b16af53a88ab2bf4c8bae7c12c4442578c7
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-12-18 21:41:40 +0100

    ENH use gnu parallel to process debug dot files

--- tools/dbg.sh
+++ tools/dbg.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/zsh
 
-for i in dbg/bprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
-for i in dbg/fprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
-geeqie dbg/fprop-0*/*.png dbg/bprop-0*/*.png
+gp=$(which parallel)
+if [ -x "$gp" ]; then
+    cd dbg
+    find -name '*.dot' | parallel "dot -Tpng {} > {.}.png"
+    #for i in dbg/bprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
+    #for i in dbg/fprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
+    if [ -d bprop-000 ] ; then
+        geeqie fprop-*/*.png bprop-*/*.png
+    else
+        geeqie fprop-*/*.png 
+    fi
+else
+    echo "Please install GNU parallel, e.g. sudo aptitude install parallel"
+fi
