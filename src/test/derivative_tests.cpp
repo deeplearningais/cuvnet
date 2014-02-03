1b24da98a1cfa7289164bb40068f352da3d1e9dd
Tobias Hartmann <hartman3@cs.uni-bonn.de>
2014-02-03 11:33:34 +0100

    ENH added tests for concatenate op
    Signed-off-by: Tobias Hartmann <hartman3@cs.uni-bonn.de>

--- src/test/derivative_tests.cpp
+++ src/test/derivative_tests.cpp
@@ -1730,7 +1730,7 @@ cuv::safeThreadSync();
 
 
 
-static float logAddExp_test(float t, float u){
+
         const float diff = (float)t - (float) u;
         if(diff > 0)
             return t + log1pf(expf(-diff));
