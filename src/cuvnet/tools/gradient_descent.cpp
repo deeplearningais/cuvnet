960f6be3a7242aac8f91f21fc4897b355f90f122
Tobias Hartmann <hartman3@cs.uni-bonn.de>
2014-02-27 15:38:10 +0100

    FIX fixed logging
    Signed-off-by: Tobias Hartmann <hartman3@cs.uni-bonn.de>

--- src/cuvnet/tools/gradient_descent.cpp
+++ src/cuvnet/tools/gradient_descent.cpp
@@ -308,12 +308,13 @@ namespace cuvnet
                 batchids[i] = i;
         }
         
+
         log4cxx::LoggerPtr log(log4cxx::Logger::getLogger("gd"));
         unsigned long int iter = 1;
         unsigned long int wups = 0;
         try{
             unsigned long int t_start = time(NULL);
-            for (; ; ++m_epoch) {
+            for (; ; m_epoch +=1) {
                 float c_err = 0;
                 float s_err = 0;
                 
@@ -379,33 +380,32 @@ namespace cuvnet
                         cuv::apply_scalar_functor(*SM, cuv::SF_EXP);
                         cuv::apply_scalar_functor(*S, cuv::SF_EXP);
                         
-                        std::cout << std::endl << "S: " << cuv::mean(*S) << std::endl;
-                        std::cout << "SM: " << cuv::mean(*SM) << std::endl;
-
+//                        std::cout << std::endl << "S: " << cuv::mean(*S) << std::endl;
+//                        std::cout << "SM: " << cuv::mean(*SM) << std::endl;
                         
                         cuv::apply_binary_functor(*SM, *S, cuv::BF_SUBTRACT);
                         cuv::apply_scalar_functor(*SM, *SM, cuv::SF_ABS);                             
                         float tmp_err = cuv::mean(*SM);
                         s_err += tmp_err;                        
-                        std::cout << "spn err: "  << tmp_err << std::endl;
-
+/*
                         std::string name = "class_";
                         name.append(std::to_string(batchids[batch]));
                         name.append("_");
                         name.append(std::to_string(m_epoch));
 
-//                        tofile(name, *classification);
-//                        tofile("labels", *Y_oneOutOfN);
-                        
-                        cuv::reduce_to_col(*a1, *classification,cuv::RF_ARGMAX);
+                        tofile(name, *classification);
+                        tofile("labels", *Y_oneOutOfN);
+*/                        
+                        cuv::apply_scalar_functor(*classification, cuv::SF_EXP);                        
+                        cuv::reduce_to_col(*a1, *classification, cuv::RF_ARGMAX);
                         cuv::reduce_to_col(*a2, *Y_oneOutOfN, cuv::RF_ARGMAX);
                         
                         *a1 -= *a2;
                         int n_wrong = batch_size - cuv::count(*a1,0);
                         tmp_err = n_wrong / (float) batch_size;
                         c_err += tmp_err;
-                        std::cout << "classification err: "  << tmp_err << std::endl;
-                        
+
+  
                         // nan check
                         if ((s_err != s_err) || (c_err != c_err)) throw std::runtime_error("NAN occured -> Abort");
                         
@@ -419,10 +419,16 @@ namespace cuvnet
                             after_weight_update(wups);
                         }
                     } else{
+                 
                         after_batch(m_epoch, batchids[batch]); // should accumulate errors etc
+                        log4cxx::MDC batch_err_spn("batch_err_spn",boost::lexical_cast<std::string>(s_err/float(n_batches))); 
+                        log4cxx::MDC batch_err_class("bach_err_class",boost::lexical_cast<std::string>(c_err /float(n_batches)));                
+                        
                     }
                 }
                 //logging
+                std::cout << "spn err: "  << s_err/float(n_batches);
+                std::cout << ", classification err: "  << c_err /float(n_batches) << std::endl;                
                 log4cxx::MDC err_spn("err_spn",boost::lexical_cast<std::string>(s_err/float(n_batches))); 
                 log4cxx::MDC err_class("err_class",boost::lexical_cast<std::string>(c_err /float(n_batches)));                
                 after_epoch(m_epoch, wups); // should log error etc
