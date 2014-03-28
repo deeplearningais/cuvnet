3237720418e2d06fe7c6f1139bf8e4b36f7fd8a1
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-03-28 14:18:50 +0100

    minor monitor: do not ouput constants to console

--- src/cuvnet/tools/monitor.cpp
+++ src/cuvnet/tools/monitor.cpp
@@ -310,9 +310,9 @@ namespace cuvnet
     void monitor::simple_logging()const{
         std::cout << "\r epoch "<<m_epochs<<":"<<m_batch_presentations<<",  free_mb="<<cuv::getFreeDeviceMemory()/1024/1024<<",  ";
         typedef std::pair<std::string, std::string> ss_t;
-        BOOST_FOREACH(const ss_t& p, m_constants){
-            std::cout << p.first<<"="<<p.second<<" ";
-        }
+        //BOOST_FOREACH(const ss_t& p, m_constants){
+        //    std::cout << p.first<<"="<<p.second<<" ";
+        //}
         BOOST_FOREACH(const watchpoint* p, m_impl->m_watchpoints){
             if(p->type == WP_SCALAR_EPOCH_STATS || p->type == WP_FUNC_SCALAR_EPOCH_STATS || p->type == WP_D_SCALAR_EPOCH_STATS || p->type == WP_FULL_WEIGHT_STATS  || p->type == WP_CONV_WEIGHT_STATS){
                 std::cout  << p->name<<"="
