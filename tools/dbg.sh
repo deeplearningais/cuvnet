e5167ce75e1ee4c6c35e6b6540c04272345412f5
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-06-30 17:31:43 +0200

    DBG ubuntu version of GNU Parallel has required --gnu param

--- tools/dbg.sh
+++ tools/dbg.sh
@@ -3,7 +3,7 @@
 gp=$(which parallel)
 if [ -x "$gp" ]; then
     cd dbg
-    find -name '*.dot' | parallel "dot -Tpng {} > {.}.png"
+    find -name '*.dot' | parallel --gnu "dot -Tpng {} > {.}.png"
     #for i in dbg/bprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
     #for i in dbg/fprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
     if [ -d bprop-000 ] ; then
