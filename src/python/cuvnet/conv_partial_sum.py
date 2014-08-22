e08630c5ad49876a00cce9a2d0edea66aaae1a90
Nico Hoeft <hoeft@bigcuda3.informatik.uni-bonn.de>
2014-08-22 02:00:00 +0200

    ENH add python script to determine optimal partial_sum parameter

---
+++ src/python/cuvnet/conv_partial_sum.py
@@ -0,0 +1,44 @@
+import cuv_python as cp
+import numpy as np
+import pandas as pd
+
+cp.initCUDA(1)
+
+def determine_partial_sum(bs, fs, img, in_ch, out_ch, repeat=5, verbose=True):
+    """Returns best performing partial_sum parameter for alex' convolutions
+    
+    bs -- batch size
+    fs -- filter size
+    img -- length/height of a square image
+    in_ch -- number input maps
+    out_ch -- number output maps out_ch
+    """
+
+    import timeit
+    res = {}
+    N = 1
+    t_dst = cp.dev_tensor_float(np.zeros((in_ch,fs*fs,out_ch)))
+    t_delta = cp.dev_tensor_float(np.zeros((out_ch,img,img,bs)))
+    t_input = cp.dev_tensor_float(np.zeros((in_ch,img,img,bs)))
+    for ps in range(2**20+1):
+        def conv():
+            cp.d_conv2d_dfilt(t_dst, t_delta, t_input, -fs/2-1, 1, 1, ps)
+        f = conv
+        try:
+            if (ps == 0) or ((img*img) % ps == 0):
+                t = timeit.Timer(stmt=f)
+                total = t.repeat(number=N, repeat=repeat)
+                res[ps] = np.array(total)/N
+                if verbose:
+                    print (" ps {:>5d}: min {:>1.5f}, avg {:>1.5f}, max {:>1.5f}"
+                        .format(ps, res[ps].min(), np.average(res[ps]), res[ps].max()))
+        except Exception as inst:
+            if verbose:
+                print " ps {:>5d}: throws exception {:s}".format(ps, inst.args)
+    res_ser = pd.Series(res)
+    avg = [np.average(x) for x in res_ser]
+    idx = np.argmin(avg)
+    opt = res_ser.index[idx]
+    print " optimal partial_sum:", opt
+    print "  using", np.min(avg), "s per call. Worst case", np.max(avg)/np.min(avg), "times slower."
+    return opt
