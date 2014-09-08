0a541dfa178833e91c867788e25c4ab6c34ed923
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-09-08 11:29:02 +0200

    ldpc dataset: back to 32768 size version

--- src/datasets/ldpc.hpp
+++ src/datasets/ldpc.hpp
@@ -20,23 +20,23 @@ namespace cuvnet
             namespace fs = boost::filesystem;
             using namespace cuv;
             std::cout << "Reading LDPC dataset..."<<std::flush;
-            std::string ptrain = (path + "/ds_32768x30_float32.bin");
-            //std::string ptrain = (path + "/ds_60000x36_float32.bin");
+            std::string ptrain = (path + "/ds_32768x30_float32.bin"); int n = 32768, m=15;
+            //std::string ptrain = (path + "/ds_60000x36_float32.bin"); int n = 60000;
             unsigned int n_test = 10000;
-            assert(fs::exists(ptrain));
+            cuvAssert(fs::exists(ptrain));
             std::ifstream ftraind(ptrain.c_str(),std::ios::in | std::ios::binary); // image data
-            assert(ftraind.is_open());
+            cuvAssert(ftraind.is_open());
 
-            cuv::tensor<float,cuv::host_memory_space> traind(cuv::extents[32768][15*2]);
-            ftraind.read((char*)traind.ptr(), sizeof(float)*traind.size()); assert(ftraind.good());
+            cuv::tensor<float,cuv::host_memory_space> traind(cuv::extents[n][m*2]);
+            ftraind.read((char*)traind.ptr(), sizeof(float)*traind.size()); cuvAssert(ftraind.good());
 
-            train_data = traind[indices[index_range(0,32768-n_test)][index_range()]].copy();
-            test_data  = traind[indices[index_range(32768-n_test,32768)][index_range()]].copy();
+            train_data = traind[indices[index_range(0,n-n_test)][index_range()]].copy();
+            test_data  = traind[indices[index_range(n-n_test,n)][index_range()]].copy();
 
-            //train_labels.resize(cuv::extents[32768-n_test][15]);
-            //test_labels.resize( cuv::extents[n_test][15]);
-            train_labels = train_data[indices[index_range()][index_range(0,15)]].copy();
-            test_labels  = test_data[indices[index_range()][index_range(0,15)]].copy();
+            train_labels.resize(cuv::extents[n-n_test][m]);
+            test_labels.resize( cuv::extents[n_test][m]);
+            //train_labels = train_data[indices[index_range()][index_range()]].copy();
+            //test_labels  = test_data[indices[index_range()][index_range()]].copy();
 
             channels = 1;
             binary   = true;
