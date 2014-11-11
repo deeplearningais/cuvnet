658259655dcc31932438771abd5f6ed9af2df684
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-11-11 12:28:43 +0100

    FIX Tobi's projection on ball

--- src/cuvnet/tools/normalization.hpp
+++ src/cuvnet/tools/normalization.hpp
@@ -86,103 +86,54 @@ namespace cuvnet
      * @param logspace ( weights are in logspace => expf(w) i projected
      */
     template<class M>
-    void project_on_unit_ball(cuv::tensor<float, M>& W, int axis, float thresh, bool logspace){
-        cuv::tensor<float, M> C = W;
+        void project_on_unit_ball(cuv::tensor<float, M>& W, int axis, float thresh, bool logspace){
+            cuv::tensor<float, M> C = W;
 
-	bool inBall = true;
+            if(axis < 0)
+                axis = W.ndim()-1;
+            if(W.ndim() < 2)
+                throw std::runtime_error("project_to_unit_ball: need >2-dimensional matrix, this one has " + boost::lexical_cast<std::string>(W.ndim()));
+            if(axis >= W.ndim())
+                throw std::runtime_error("project_to_unit_ball: axis parameter is not in matrix dimensions, of which there are " + boost::lexical_cast<std::string>(W.ndim()));
 
-        if(axis < 0)
-            axis = W.ndim()-1;
-        if(W.ndim() < 2)
-            throw std::runtime_error("project_to_unit_ball: need >2-dimensional matrix, this one has " + boost::lexical_cast<std::string>(W.ndim()));
-        if(axis >= W.ndim())
-            throw std::runtime_error("project_to_unit_ball: axis parameter is not in matrix dimensions, of which there are " + boost::lexical_cast<std::string>(W.ndim()));
+            unsigned int ax_shape = W.shape(axis);
+            if(W.ndim() > 2){
+                unsigned int other_shape = W.size() / ax_shape;
+                if(axis == W.ndim()-1){
+                    axis = 1;
+                    C.reshape(cuv::extents[other_shape][ax_shape]);
+                }else if(axis == 0){
+                    axis = 0;
+                    C.reshape(cuv::extents[ax_shape][other_shape]);
+                }else{
+                    throw std::runtime_error("project_to_unit_ball: cannot normalize intermediate (0<axis<ndim-1) axis");
+                }
+            }
+            //ax_shape = C.shape(1-axis);
+            cuv::tensor<float, M> ax(ax_shape);
+            if(logspace){
+                if(axis == 1)     { cuv::reduce_to_row(ax, C, cuv::RF_MAX); }
+                else if(axis == 0){ cuv::reduce_to_col(ax, C, cuv::RF_MAX); }
 
-        if((C.ndim() == 3 || C.ndim() == 4) && (C.shape(axis) % 16 == 0))
-        {
-            cuv::alex_conv::project_to_ball(C, thresh);
-            return;
-        }
-        unsigned int ax_shape = W.shape(axis);
-        if(W.ndim() > 2){
-            unsigned int other_shape = W.size() / ax_shape;
-            if(axis == W.ndim()-1){
-                axis = 1;
-                C.reshape(cuv::extents[other_shape][ax_shape]);
-            }else if(axis == 0){
-                axis = 0;
-                C.reshape(cuv::extents[ax_shape][other_shape]);
-            }else{
-                throw std::runtime_error("project_to_unit_ball: cannot normalize intermediate (0<axis<ndim-1) axis");
+                cuv::apply_scalar_functor(ax, cuv::SF_NEGATE);
+                cuv::matrix_op_vec(C, C, ax, axis, cuv::BF_ADD); 
+                cuv::apply_scalar_functor(C, cuv::SF_EXP);
+            }
+            if(axis == 1)     { cuv::reduce_to_row(ax, C, cuv::RF_ADD_SQUARED); }
+            else if(axis == 0){ cuv::reduce_to_col(ax, C, cuv::RF_ADD_SQUARED); }
+
+            cuv::apply_scalar_functor(ax, cuv::SF_SQRT);
+
+            if(thresh != 1.f)
+                cuv::apply_scalar_functor(ax, cuv::SF_MULT, 1.f / thresh); // ax[over_thresh] *= 1/thresh
+
+            if(axis == 1)     { cuv::matrix_divide_row(C, ax); }
+            else if(axis == 0){ cuv::matrix_divide_col(C, ax); }
+
+            if(logspace){
+                cuv::apply_scalar_functor(C, cuv::SF_LOG);
             }
         }
-        //ax_shape = C.shape(1-axis);
-        cuv::tensor<float, M> ax(ax_shape);
-        cuv::tensor<unsigned char, M> over_thresh(ax_shape);
-        if(axis == 1){
-	    if (logspace){
-                cuv::reduce_to_row(ax, C, cuv::RF_LOGADDEXP);
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_EXP);
-	        if (inBall)
-		     cuv::apply_scalar_functor(ax, ax, cuv::SF_SQUARE);
-	    }
-            else {
-		if ( inBall)
-	            cuv::reduce_to_col(ax, C, cuv::RF_ADD_SQUARED);
-	        else
-                    cuv::reduce_to_col(ax, C, cuv::RF_ADD);
-	    }
-        }
-        else if(axis == 0){
-	    if (logspace){
-                cuv::reduce_to_col(ax, C, cuv::RF_LOGADDEXP);
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_EXP);
-	        if (inBall)
-		       	cuv::apply_scalar_functor(ax, ax, cuv::SF_SQUARE);
-	    } else {
-		if ( inBall)
-	            cuv::reduce_to_col(ax, C, cuv::RF_ADD_SQUARED);
-	        else
-	            cuv::reduce_to_col(ax, C, cuv::RF_ADD);
-	    }
-        }
-
-        cuv::apply_scalar_functor(over_thresh, ax, cuv::SF_GT, thresh * thresh);
-        {
-            float n_over_thresh = cuv::sum(over_thresh);
-            if(n_over_thresh > 0.f){
-                //std::cout << "n_over_thresh:" << n_over_thresh << std::endl;
-            }else{
-                //std::cout << "cuv::mean(ax):" << cuv::mean(ax) << std::endl;
-            }
-            if(n_over_thresh < 0.1f)
-                return;
-        }
-        if (inBall) 
-   	    cuv::apply_scalar_functor(ax, cuv::SF_SQRT); // ax[over_thresh] = sqrt(ax[over_thresh])
-        cuv::apply_scalar_functor(ax, cuv::SF_MULT, 1.f / thresh);      // ax[over_thresh] *= 1/thresh
-        over_thresh = !over_thresh;    // 
-        cuv::apply_scalar_functor(ax, cuv::SF_MULT, 0.f, &over_thresh); // ax[!over_thresh] = 0
-        cuv::apply_scalar_functor(ax, cuv::SF_ADD , 1.f, &over_thresh); // ax[!over_thresh] += 1
-     
-
-      	if(axis == 1){
-            if ( logspace ){
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_LOG);
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_NEGATE);
-                cuv::matrix_plus_row(C, ax);
-	    } else
-                cuv::matrix_divide_row(C, ax);
-	}
-        else if(axis == 0){
-            if ( logspace ){
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_LOG);
-	        cuv::apply_scalar_functor(ax, ax, cuv::SF_NEGATE);
-                cuv::matrix_plus_col(C, ax);
-	    } else
-                cuv::matrix_divide_col(C, ax);
-	}
-    }
 
 
 
