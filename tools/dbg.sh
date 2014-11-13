26307c99a63c0657efdf2f29471f177b52e51bf8
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-11-13 17:57:30 +0100

    ENH add path for dbg.sh

--- tools/dbg.sh
+++ tools/dbg.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/zsh
 
+if [[ "$1" == "--help"  ||  "$1" == ""  ||  "$1" == "-h" ]]; then
+    echo "Usage: $0 {path}"
+    echo "  where path is the base path to the experiment, containing the dbg folder"
+    exit 0
+fi
+
 gp=$(which parallel)
 if [ -x "$gp" ]; then
-    cd dbg
+    cd $1/dbg
     find -name '*.dot' | parallel --gnu "dot -Tpng {} > {.}.png"
     #for i in dbg/bprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
     #for i in dbg/fprop-0*/*.dot ; do dot -Tpng $i > ${i/.dot/.png} ; done 
