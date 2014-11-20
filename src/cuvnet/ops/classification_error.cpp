a914a0e90dde397a1bbb2bba12827c0235000ac4
Nico Hoeft <hoeft@bigcuda3.informatik.uni-bonn.de>
2014-11-20 16:40:59 +0100

    FIX classification loss for >2 dims

--- src/cuvnet/ops/classification_error.cpp
+++ src/cuvnet/ops/classification_error.cpp
@@ -41,51 +41,50 @@ namespace cuvnet
         unsigned int dim_other_axes;
         unsigned int batch_size;
         if(m_axis == 0) { 
-            dim_other_axes = inp0.size() / inp0.shape(inp0.ndim()-1);
-            inp0.reshape(dim_other_axes, inp0.shape(inp0.ndim()-1));
-            inp1.reshape(dim_other_axes, inp0.shape(inp0.ndim()-1));
-            batch_size = inp0.shape(0);
-        } else {
             dim_other_axes = inp0.size() / inp0.shape(0);
             inp0.reshape(inp0.shape(0), dim_other_axes);
             inp1.reshape(inp0.shape(0), dim_other_axes);
             batch_size = inp0.shape(1);
+        } else {
+            dim_other_axes = inp0.size() / inp0.shape(inp0.ndim()-1);
+            inp0.reshape(dim_other_axes, inp0.shape(inp0.ndim()-1));
+            inp1.reshape(dim_other_axes, inp0.shape(inp0.ndim()-1));
+            batch_size = inp0.shape(0);
         }
 
         // Apply ignore mask if needed
         value_type& inp0src = inp0;
         float avg_ign = 0;
         matrix a_ign ( batch_size, cuvnet::get_global_allocator() );
+
+        value_type inp2;
         if (ignore) {
             param_t::element_type& p2 = *m_params[2];
-            value_type inp2 = p2.value.cdata();
+            inp2 = p2.value.cdata();
 
             // reduce ignore term over classes.
-            // TODO the ignore input should NOT have multiple classes!!!
-            if (m_axis == 0) inp2.reshape(dim_other_axes, 1);
-            else             inp2.reshape(1, dim_other_axes);
-            if(m_axis == 0)  cuv::reduce_to_col(a_ign, inp2, cuv::RF_MEAN);
-            else             cuv::reduce_to_row(a_ign, inp2, cuv::RF_MEAN);
+            if (m_axis == 0) inp2.reshape(1, dim_other_axes);
+            else             inp2.reshape(dim_other_axes, 1);
 
             // determine amount of ignored part
-            avg_ign = cuv::mean(a_ign);
+            avg_ign = cuv::mean(inp2);
         }
 
         matrix a1 ( batch_size, cuvnet::get_global_allocator() );
         matrix a2 ( batch_size, cuvnet::get_global_allocator() );
         if(m_axis == 0) {
-            cuv::reduce_to_col(a1, inp0src,cuv::RF_ARGMAX);
-            cuv::reduce_to_col(a2, inp1,cuv::RF_ARGMAX);
-        } else {
             cuv::reduce_to_row(a1, inp0src,cuv::RF_ARGMAX);
             cuv::reduce_to_row(a2, inp1,cuv::RF_ARGMAX);
+        } else {
+            cuv::reduce_to_col(a1, inp0src,cuv::RF_ARGMAX);
+            cuv::reduce_to_col(a2, inp1,cuv::RF_ARGMAX);
         }
 
         a1 -= a2;
         cuv::apply_scalar_functor(a1, cuv::SF_ABS);
         cuv::apply_scalar_functor(a1, cuv::SF_GT, 0.5f);
         if(ignore)
-            a1 *= a_ign;
+            a1 *= inp2;
 
         float n_wrong = cuv::sum(a1);
 
