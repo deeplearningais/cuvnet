e076fccdba7de8ebf680563337fc9439a0bf3ad2
Hannes Schulz <schulz@ais.uni-bonn.de>
2015-02-03 00:21:35 +0100

    FIX monitor div by zero in some cases

--- src/cuvnet/tools/monitor.cpp
+++ src/cuvnet/tools/monitor.cpp
@@ -209,7 +209,7 @@ namespace cuvnet
                 p->notify_scalar_stats((float)p->func.evaluate()[0], m_cv_mode);
             }
             if(p->type == WP_SINK_ONCE_STATS && 
-                    ((m_every == 0 && bid == 0) || (m_batch_presentations % m_every == 0)))
+                    ((m_every == 0 && bid == 0) || (m_every != 0 && m_batch_presentations % m_every == 0)))
             {
                 update_stats(p->stats(m_cv_mode), p->sink->cdata());
             }
