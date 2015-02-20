0746c84da394002b23cee67b5b9ad98fe1359f03
Hannes Schulz <schulz@ais.uni-bonn.de>
2015-02-20 14:43:17 +0100

    TST detection dataset: /show/ visualization

--- src/test/data_source.cpp
+++ src/test/data_source.cpp
@@ -6,6 +6,7 @@
 
 #include <cuv/basics/tensor.hpp>
 #include <cuvnet/datasets/detection.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -14,7 +15,7 @@ BOOST_AUTO_TEST_SUITE( t_detection )
 BOOST_AUTO_TEST_CASE( loading ){
     using namespace datasets;
 
-    rgb_detection_dataset ids("image_dataset.txt", 224, 1);
+    rgb_detection_dataset ids("image_dataset.txt", 512, 1);
     BOOST_REQUIRE_EQUAL(2, ids.m_meta.size());
     BOOST_CHECK_NE(std::string::npos, ids.m_meta[0].rgb_filename.find("2008_007573.jpg"));
     BOOST_CHECK_NE(std::string::npos, ids.m_meta[1].rgb_filename.find("2008_007576.jpg"));
@@ -27,7 +28,9 @@ BOOST_AUTO_TEST_CASE( loading ){
         = ids.next(1);
     
     meta_data<rgb_detection_tag>::show("img0", *patset0->m_todo[0]);
+    cv::waitKey(0);
     meta_data<rgb_detection_tag>::show("img1", *patset1->m_todo[0]);
+    cv::waitKey(0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
