bd9e2f9dbe374b181404e8626ad84253d5549a51
Nico Hoeft <hoeft@bigcuda1.informatik.uni-bonn.de>
2015-03-02 23:50:12 +0100

    ADD rgbd_detection dataset and loader

--- src/cuvnet/datasets/cv_loader.hpp
+++ src/cuvnet/datasets/cv_loader.hpp
@@ -138,6 +138,81 @@ namespace datasets
             }
             std::cout << "."<<std::flush;
         }
+
     };
+
+    struct rgbd_detection_loader{
+        ThreadPool m_pool;
+        typedef cuvnet::image_queue<rgbd_detection_dataset> queue_t;
+        rgbd_detection_dataset m_trainset, m_valset;
+        cuvnet::image_queue<rgbd_detection_dataset> m_trainqueue, m_valqueue;
+        std::list<boost::shared_ptr<rgbd_detection_dataset::pattern_t> > m_open_list;
+        std::list<cuvnet::cv_mode> m_open_list_mode;
+
+        rgbd_detection_loader(std::string basename, int n_jobs=0)
+        : m_pool(n_jobs)
+        , m_trainset(basename + "_train.txt", 224, 1)
+        , m_valset(basename + "_val.txt", 224, 5)
+        , m_trainqueue(m_pool, m_trainset, 128, 3*128)
+        , m_valqueue(m_pool, m_valset, 12, 3*12)
+        {
+        }
+        void load_instance(queue_t& q, int i, cuvnet::matrix& rgb, cuvnet::matrix& depth, std::vector<bbox>& tch){
+            auto pat = q.pop();
+            rgb[cuv::indices[i]] = pat->rgb;
+            depth[cuv::indices[i]] = pat->depth;
+            tch = pat->bboxes;
+            m_open_list.push_back(pat);
+        }
+
+        void save_instance(const std::vector<bbox>& pred){
+            assert(!m_open_list.empty());
+            auto pat = m_open_list.front();
+            m_open_list.pop_front();
+            cuvnet::cv_mode mode = m_open_list_mode.front();
+            m_open_list_mode.pop_front();
+
+            pat->predicted_bboxes = pred;
+
+            // dispatch to correct set
+            if(mode == cuvnet::CM_TRAIN)
+                m_trainset.notify_done(pat);
+            else 
+                m_valset.notify_done(pat);
+        }
+
+        void save_batch(const std::vector<std::vector<bbox> >& pred){
+            int batch_size = pred.size();
+            for(int i=0; i < batch_size; i++)
+                save_instance(pred[i]);
+        }
+
+        void load_batch(cuvnet::cv_mode mode, cuvnet::matrix& rgb, cuvnet::matrix& depth, std::vector<std::vector<bbox> >& tch){
+            m_open_list.clear();
+            m_open_list_mode.clear();
+            int batch_size = rgb.shape(0);
+            if(mode == cuvnet::CM_TRAINALL)
+                mode = drand48() > (m_trainset.size() / ((float)(m_trainset.size() + m_valset.size())))
+                    ? cuvnet::CM_VALID
+                    : cuvnet::CM_TRAIN;
+
+            tch.resize(batch_size);
+
+            for(int i=0; i < batch_size; i++){
+                if(mode == cuvnet::CM_TRAIN)
+                    load_instance(m_trainqueue, i, rgb, depth, tch[i]);
+                else if(mode == cuvnet::CM_VALID || mode == cuvnet::CM_TEST)
+                    load_instance(m_valqueue, i, rgb, depth, tch[i]);
+                
+                // DEBUG
+                //cuv::fill_rnd_uniform(rgb);
+                
+                m_open_list_mode.push_back(mode);
+            }
+            std::cout << "d"<<std::flush;
+        }
+
+    };
+
 
 }
--- src/cuvnet/datasets/detection.hpp
+++ src/cuvnet/datasets/detection.hpp
@@ -11,6 +11,7 @@ namespace datasets
 
     /// a tag for object detection tasks
     struct rgb_detection_tag{};
+    struct rgbd_detection_tag{};
 
     /// represents a bounding box of an object
     struct bbox{
@@ -36,6 +37,10 @@ namespace datasets
         std::vector<bbox> bboxes;
         std::vector<bbox> predicted_bboxes;
     };
+    struct rgbd_detection_pattern : public rgbd_pattern {
+        std::vector<bbox> bboxes;
+        std::vector<bbox> predicted_bboxes;
+    };
 
     /// meta infos for detection
     template<>
@@ -44,6 +49,17 @@ namespace datasets
             std::vector<bbox> bboxes;
             typedef rgb_image input_t;
             typedef pattern_with_original<rgb_detection_pattern, input_t> pattern_t;
+            typedef pattern_t::patternset_t patternset_t;
+
+            static void show(std::string name, const pattern_t& pat);
+        };
+    template<>
+        struct meta_data<rgbd_detection_tag>{
+            std::string rgb_filename;
+            std::string depth_filename;
+            std::vector<bbox> bboxes;
+            typedef rgbd_image input_t;
+            typedef pattern_with_original<rgbd_detection_pattern, input_t> pattern_t;
             typedef pattern_t::patternset_t patternset_t;
 
             static void show(std::string name, const pattern_t& pat);
@@ -109,6 +125,66 @@ namespace datasets
         void aggregate_statistics(const unsigned int n = 1000) override;
     };
 
+    struct rgbd_detection_dataset : public image_dataset<rgbd_detection_tag> {
+        typedef image_dataset<rgbd_detection_tag> base_t;   ///< type of own base class
+        rgbd_detection_dataset(const std::string& filename, int pattern_size, int n_crops);
+        
+        /// the mean image, which is subtracted from all patterns before passing them to the model
+        cuv::tensor<float, cuv::host_memory_space> m_imagenet_mean;
+        cuv::tensor<float, cuv::host_memory_space> m_imagenet_mean_depth;
+
+        /// number of crops generated for every image
+        int m_n_crops;
+        /// size of the crops generated
+        int m_pattern_size;
+        /// filename pointing to meta information
+        std::string m_filename;
+        /// if true, cover image completely.
+        bool m_exhaustive;
+
+        /// number of different object classes
+        unsigned int m_n_classes;
+        /// the names of all the classes
+        std::vector<std::string> m_class_names;
+        /// @return number of different object classes
+        inline unsigned int n_classes(){ return m_n_classes; }
+
+        /// path to be prepended to paths in dataset file
+        std::string m_image_basepath;
+
+        /**
+         * takes an image, crops (random) region(s) and returns the result as a cuv tensor.
+         */
+        boost::shared_ptr<patternset_t> preprocess(size_t idx, boost::shared_ptr<input_t> in) const override;
+
+        /**
+         * Records the model prediction.
+         */
+        void notify_done(boost::shared_ptr<pattern_t> pat) override;
+
+        /**
+         * set a binary file containing a 4xNxN float32 C-ordered imagenet mean.
+         *
+         * where N is the pattern_size supplied to the constructor.
+         */
+        void set_imagenet_mean(std::string filename);
+
+        /**
+         * Set path prepended to path in dataset file.
+         * @note this is destructive and cannot be called twice.
+         */
+        void set_image_basepath(std::string path);
+
+        /**
+         * clears all predictions.
+         */
+        void clear_predictions();
+
+        /**
+         * generates n pattern ad accumulates statistics over them
+         */
+        void aggregate_statistics(const unsigned int n = 1000) override;
+    };
     /// @}
 }
 #endif /* __CUVNET_DETECTION_HPP__isfhaasdfhksjdhf */
